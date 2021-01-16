3ea341f2035cb1543bad0b4e65ff2884d4fa9d6b
mu <59917266+4eUeP@users.noreply.github.com>
2021-01-16 16:58:42 +0800

    add submodules for Z project

--- script/format.sh
+++ script/format.sh
@@ -2,5 +2,8 @@
 
 FORMATER_BIN=${FORMATER_BIN:-stylish-haskell}
 
-find . -type f -not -path "*/dist-newstyle/*" -not -path "*/.stack-work/*" | \
-    grep "\.l\?hs$" | xargs $FORMATER_BIN -c .stylish-haskell.yaml -i
+find . -type f \
+    -not -path "*/dist-newstyle/*" \
+    -not -path "*/.stack-work/*" \
+    -not -path "*/z-*" \
+    | grep "\.l\?hs$" | xargs $FORMATER_BIN -c .stylish-haskell.yaml -i
