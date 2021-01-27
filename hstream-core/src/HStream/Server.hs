a582b0ffa9751dc84893371c2d20c78d155bb2eb
mu <59917266+4eUeP@users.noreply.github.com>
2021-01-27 09:57:32 +0800

    remove hstream-core pkg

--- hstream-core/src/HStream/Server.hs
+++
@@ -1,7 +0,0 @@
-module HStream.Server
-  ( module HStream.Server.Types
-  , module HStream.Server.Command
-  ) where
-
-import           HStream.Server.Command
-import           HStream.Server.Types
