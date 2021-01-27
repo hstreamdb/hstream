f1d14d72f3f54d4bfee96cc3c3857279ae5b7e17
EMQ-YangM <yangm@emqx.io>
2021-01-27 09:13:32 +0800

    add checkpoint functions

--- hstream-store/HStream/Store/Stream.hs
+++ hstream-store/HStream/Store/Stream.hs
@@ -9,7 +9,7 @@ module HStream.Store.Stream
   , getMaxPayloadSize
 
     -- ** Sequence Number
-  , SequenceNum
+  , SequenceNum(..)
   , FFI.sequenceNumInvalid
     -- ** Data Record
   , DataRecord (..)
