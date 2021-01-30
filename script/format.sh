c8476f9fd5027312abeb47d49de40838fb3d4863
Wangbin <wangbin@emqx.io>
2021-01-30 15:55:34 +0800

    chore: update script/format.sh for hstream-processing

--- script/format.sh
+++ script/format.sh
@@ -18,7 +18,13 @@ if [ "$1" == "ci" ] || [ -z "$1" ]; then
         | grep "\.l\?hs$" | xargs $FORMATER_BIN -c .stylish-haskell.yaml -i
     find ./hstream-processing -type f | grep "\.l\?hs$" | xargs $FORMATER_BIN -c ./hstream-processing/.stylish-haskell.yaml -i
 elif [ "$1" == "processing" ]; then
-    find ./hstream-processing -type f | grep "\.l\?hs$" | xargs ormolu -m inplace
-    find ./hstream-processing -type f | grep "\.l\?hs$" | xargs $FORMATER_BIN -c ./hstream-processing/.stylish-haskell.yaml -i
+    find ./hstream-processing -type f \
+        -not -path "*/dist-newstyle/*" \
+        -not -path "*/.stack-work/*" \
+        | grep "\.l\?hs$" | xargs ormolu -m inplace
+    find ./hstream-processing -type f  \
+        -not -path "*/dist-newstyle/*" \
+        -not -path "*/.stack-work/*" \
+        | grep "\.l\?hs$" | xargs $FORMATER_BIN -c ./hstream-processing/.stylish-haskell.yaml -i
     cabal-fmt --inplace ./hstream-processing/hstream-processing.cabal
 fi
