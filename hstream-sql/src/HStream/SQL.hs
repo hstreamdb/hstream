957797268e86dc7a51d97a68d2c64c119bcf68c5
Commelina <maosics@gmail.com>
2021-04-20 15:55:21 +0800

    hstream-sql: move Codegen module to hstream-sql package

--- hstream-sql/src/HStream/SQL.hs
+++ hstream-sql/src/HStream/SQL.hs
@@ -2,8 +2,10 @@ module HStream.SQL
   ( module HStream.SQL.Abs
   , module HStream.SQL.AST
   , module HStream.SQL.Parse
+  , module HStream.SQL.Codegen
   ) where
 
 import           HStream.SQL.AST
 import           HStream.SQL.Abs
 import           HStream.SQL.Parse
+import           HStream.SQL.Codegen
