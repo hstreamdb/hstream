b5d84b9f31b99ea39285bde1c0e5da1371b970ae
Time-Hu <husj@emqx.io>
2021-05-10 16:48:11 +0800

    simply formatted error message

--- hstream/HStream/Format.hs
+++ hstream/HStream/Format.hs
@@ -1,10 +1,17 @@
+{-# LANGUAGE RecordWildCards #-}
 module HStream.Format where
 
-import qualified Data.Aeson          as A
-import qualified Data.Aeson.Text     as A
-import qualified Data.HashMap.Strict as HM
-import qualified Data.Text           as T
-import qualified Data.Text.Lazy      as TL
+import qualified Data.Aeson                        as A
+import qualified Data.Aeson.Text                   as A
+import qualified Data.HashMap.Strict               as HM
+import qualified Data.Map.Strict                   as M
+import           Data.Maybe                        (maybeToList)
+import qualified Data.Text                         as T
+import qualified Data.Text.Lazy                    as TL
+import qualified Data.Vector                       as V
+import qualified HStream.SQL.Exception             as E
+import qualified HStream.Server.HStreamApi         as HA
+import qualified ThirdParty.Google.Protobuf.Struct as P
 
 type Width = Int
 type Box = (Width, String)
@@ -52,3 +59,39 @@ renderJSONObjectToTable hmap =
 renderJSONToTable :: A.Value -> Table
 renderJSONToTable (A.Object hmap) = renderJSONObjectToTable hmap
 renderJSONToTable x = emptyTable `addRow` (emptyRow `addBox` (box . show) x)
+
+--------------------------------------------------------------------------------------------------
+formatSomeSQLException :: E.SomeSQLException -> String
+formatSomeSQLException (E.ParseException   info) = "Parse exception " ++ formatParseExceptionInfo info ++ "\n"
+formatSomeSQLException (E.RefineException  info) = "Refine exception at " ++ show info ++ "\n"
+formatSomeSQLException (E.CodegenException info) = "Codegen exception at " ++ show info ++ "\n"
+
+formatParseExceptionInfo :: E.SomeSQLExceptionInfo -> String
+formatParseExceptionInfo E.SomeSQLExceptionInfo{..} =
+  case words sqlExceptionMessage of
+    "syntax" : "error" : "at" : "line" : x : "column" : y : ss  -> "at <line " ++ x ++ "column " ++ y ++ ">: syntax error " ++ unwords ss ++ "."
+    _ -> posInfo ++ sqlExceptionMessage ++ "."
+ where
+   posInfo = case sqlExceptionPosition of
+      Just (l,c) -> "at <line " ++ show l ++ ", column " ++ show c ++ ">"
+      Nothing    -> ""
+
+formatCommandQueryResponse :: HA.CommandQueryResponse -> String
+formatCommandQueryResponse (HA.CommandQueryResponse (Just x)) = case x of
+  HA.CommandQueryResponseKindSuccess _ -> "Command successfully executed.\n"
+  HA.CommandQueryResponseKindResultSet (HA.CommandQueryResultSet y) -> (unlines . map ( (++ ".") .formatStruct) . V.toList) y
+
+formatStruct :: P.Struct -> String
+formatStruct (P.Struct kv) = unwords . map (\(x, y) -> TL.unpack x ++ (' ' : (concat . maybeToList) y)) . M.toList . fmap (fmap formatValue) $ kv
+
+formatValue :: P.Value -> String
+formatValue (P.Value Nothing)  = ""
+formatValue (P.Value (Just x)) = formatValueKind x
+
+formatValueKind :: P.ValueKind -> String
+formatValueKind (P.ValueKindNullValue _) = "NULL"
+formatValueKind (P.ValueKindNumberValue n) = show n
+formatValueKind (P.ValueKindStringValue s) = TL.unpack s
+formatValueKind (P.ValueKindBoolValue   b) = show b
+formatValueKind (P.ValueKindStructValue struct) = formatStruct struct
+formatValueKind (P.ValueKindListValue (P.ListValue vs)) = unwords . map formatValue . V.toList $ vs
