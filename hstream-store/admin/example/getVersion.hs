07426cc91cefdcaa73f5c113cd23c8165d09efe6
mu <59917266+4eUeP@users.noreply.github.com>
2021-05-11 11:03:43 +0800

    mv ldadmin to hstore-admin

--- hstream-store/admin/example/getVersion.hs
+++ hstream-store/admin/example/getVersion.hs
@@ -3,16 +3,19 @@
 module Main where
 
 import qualified Admin.AdminAPI.Client        as Admin
-import           Data.Text                    (Text)
-import           Fb303.FacebookService.Client (getVersion)
+import           Control.Monad.IO.Class       (liftIO)
+import           Fb303.FacebookService.Client (getPid, getVersion)
 import           Thrift.Channel.SocketChannel (SocketConfig (..), localhost,
                                                withSocketChannel)
 import           Thrift.Protocol.Id           (binaryProtocolId)
 
-main :: IO Text
+main :: IO ()
 main = do
   -- change to your lodevice admin server host & port
   let (host, port) = (localhost, 47440)
-
   let socketConfig = SocketConfig host port binaryProtocolId
-  withSocketChannel @Admin.AdminAPI socketConfig getVersion
+
+  withSocketChannel @Admin.AdminAPI socketConfig $ do
+    pid <- getPid
+    version <- getVersion
+    liftIO $ print (pid, version)
