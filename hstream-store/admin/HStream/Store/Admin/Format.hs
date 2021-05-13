284d2c47f82a088cbb435ef8bb24339157ac5bca
mu <59917266+4eUeP@users.noreply.github.com>
2021-05-13 16:02:53 +0800

    an initial of hstore-admin

---
+++ hstream-store/admin/HStream/Store/Admin/Format.hs
@@ -0,0 +1,17 @@
+module HStream.Store.Admin.Format
+  ( simpleShowTable
+  ) where
+
+import           Data.Default      (def)
+import qualified Text.Layout.Table as Table
+
+
+simpleShowTable :: [(String, Int, Table.Position Table.H)] -> [[String]] -> String
+simpleShowTable _ [] = ""
+simpleShowTable colconfs rols =
+  let titles = map (\(t, _, _) -> t) colconfs
+      colout = map (\(_, maxlen, pos) -> Table.column (Table.expandUntil maxlen) pos def def) colconfs
+   in Table.tableString colout
+                        Table.asciiS
+                        (Table.titlesH titles)
+                        [ Table.rowsG rols ]
---
+++ hstream-store/admin/HStream/Store/Admin/Status.hs
@@ -0,0 +1,47 @@
+module HStream.Store.Admin.Status
+  (runStatus
+  ) where
+
+import           Control.Monad
+import           Data.Int
+import           Data.List                  (elemIndex, sortBy)
+import           Data.Text                  (Text)
+import qualified Data.Text                  as Text
+import qualified Text.Layout.Table          as Table
+
+import qualified HStream.Store.Admin.API    as AA
+import           HStream.Store.Admin.Format (simpleShowTable)
+import           HStream.Store.Admin.Types
+
+-- | Gets the state object for all nodes that matches the supplied NodesFilter.
+--
+-- If NodesFilter is empty we will return all nodes. If the filter does not
+-- match any nodes, an empty list of nodes is returned in the
+-- NodesStateResponse object. `force` will force this method to return all the
+-- available state even if the node is not fully ready. In this case we will
+-- not throw NodeNotReady exception but we will return partial data.
+runStatus :: AA.SocketConfig AA.AdminAPI -> StatusOpts -> IO String
+runStatus socketConfig StatusOpts{..} = do
+  states <- AA.withSocketChannel @AA.AdminAPI socketConfig $ do
+    case fromSimpleNodesFilter statusFilter of
+      [] -> AA.nodesStateResponse_states <$> AA.getNodesState (AA.NodesStateRequest Nothing (Just statusForce))
+      xs -> do
+        rs <- forM xs $ \x -> AA.nodesStateResponse_states <$> AA.getNodesState (AA.NodesStateRequest (Just x) (Just statusForce))
+        return $ concat rs
+
+  let titles = ["ID", "NAME", "STATE", "HEALTH STATUS"]
+  let getID = show . AA.nodeConfig_node_index . AA.nodeState_config
+  let getName = Text.unpack . AA.nodeConfig_name . AA.nodeState_config
+  let getState = Text.unpack . last . Text.splitOn "_" . Text.pack . show . AA.nodeState_daemon_state
+  let getHealthState = Text.unpack . last . Text.splitOn "_" . Text.pack . show . AA.nodeState_daemon_health_status
+  let collectState s = map ($ s) [getID, getName, getState, getHealthState]
+  let allStates = map collectState states
+
+  let m_sortIdx = elemIndex (Text.unpack . Text.toUpper $ statusSortField) titles
+  case m_sortIdx of
+    Just sortIdx -> do
+      let stats = sortBy (\xs ys -> compare (xs!!sortIdx) (ys!!sortIdx)) allStates
+      case statusFormat of
+        TabularFormat -> return $ simpleShowTable (map (, 10, Table.left) titles) stats
+        JSONFormat    -> errorWithoutStackTrace "NotImplemented"
+    Nothing -> errorWithoutStackTrace $ "No such sort key: " <> Text.unpack statusSortField
