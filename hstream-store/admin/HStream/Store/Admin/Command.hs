41b8840e638b402cd7c7a1be5188b51be975c53f
YangKian <1207783292@qq.com>
2021-05-26 13:41:27 +0800

    add ld command config dump

--- hstream-store/admin/HStream/Store/Admin/Command.hs
+++ hstream-store/admin/HStream/Store/Admin/Command.hs
@@ -1,7 +1,9 @@
 module HStream.Store.Admin.Command
   ( module HStream.Store.Admin.Command.NodesConfig
   , module HStream.Store.Admin.Command.Status
+  , module HStream.Store.Admin.Command.Config
   ) where
 
+import           HStream.Store.Admin.Command.Config
 import           HStream.Store.Admin.Command.NodesConfig
 import           HStream.Store.Admin.Command.Status
