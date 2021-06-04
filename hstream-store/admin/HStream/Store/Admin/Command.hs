ba3990fd61a52205ffba5b1a22bfee6c5c8a4b82
YangKian <45479280+YangKian@users.noreply.github.com>
2021-06-04 10:10:40 +0800

    implement basic function of check-impact (#208)

--- hstream-store/admin/HStream/Store/Admin/Command.hs
+++ hstream-store/admin/HStream/Store/Admin/Command.hs
@@ -3,8 +3,10 @@ module HStream.Store.Admin.Command
   , module HStream.Store.Admin.Command.Status
   , module HStream.Store.Admin.Command.Config
   , module HStream.Store.Admin.Command.Logs
+  , module HStream.Store.Admin.Command.CheckImpact
   ) where
 
+import           HStream.Store.Admin.Command.CheckImpact
 import           HStream.Store.Admin.Command.Config
 import           HStream.Store.Admin.Command.Logs
 import           HStream.Store.Admin.Command.NodesConfig
---
+++ hstream-store/admin/HStream/Store/Admin/Command/CheckImpact.hs
@@ -0,0 +1,107 @@
+module HStream.Store.Admin.Command.CheckImpact
+  ( checkImpact
+  ) where
+
+import qualified Data.Int                  as Int
+import           Data.List                 (union)
+import qualified Data.Map.Strict           as Map
+import           Data.Maybe                (fromJust, mapMaybe)
+import           Data.Set                  (Set)
+import           Data.Text                 (Text)
+
+import qualified HStream.Store.Admin.API   as AA
+import           HStream.Store.Admin.Types
+
+type NodeIdxMap = Map.Map AA.NodeIndex NodeProperties
+type NodeNameToIdxMap = Map.Map Text AA.NodeIndex
+
+data NodeProperties = NodeProperties
+  { nodeIndex :: AA.NodeIndex
+  , nodeRoles :: Set AA.Role
+  , nodeName  :: Text
+  , numShards :: Int.Int32
+  } deriving (Show)
+
+getNodeProperties :: AA.HeaderConfig AA.AdminAPI -> IO [NodeProperties]
+getNodeProperties conf = do
+  res <- AA.sendAdminApiRequest conf (AA.getNodesConfig (AA.NodesFilter Nothing Nothing Nothing))
+  return $ map build (AA.nodesConfigResponse_nodes res)
+    where
+      build :: AA.NodeConfig -> NodeProperties
+      build c = NodeProperties
+        { nodeIndex = AA.nodeConfig_node_index c
+        , nodeRoles = AA.nodeConfig_roles c
+        , nodeName = AA.nodeConfig_name c
+        , numShards = fromJust $ AA.storageConfig_num_shards <$> (AA.nodeConfig_storage c)
+        }
+
+getNodeIdxMap :: [NodeProperties] -> NodeIdxMap
+getNodeIdxMap = Map.fromList . map (\node -> (nodeIndex node, node))
+
+getNodeNameToIdxMap :: [NodeProperties] -> NodeNameToIdxMap
+getNodeNameToIdxMap = Map.fromList . map (\node -> (nodeName node, nodeIndex node))
+
+getNodesShardNum :: [AA.NodeIndex] -> NodeIdxMap -> [Int.Int32]
+getNodesShardNum idxs dict = map (numShards . fromJust . flip Map.lookup dict) idxs
+
+combine :: CheckImpactOpts -> [NodeProperties] -> AA.ShardSet
+combine impactOpts prop = do
+  let shardSets = shards impactOpts
+  let nameToIdxMap = getNodeNameToIdxMap prop
+  let nodeIdxMap = getNodeIdxMap prop
+  let nodesIdsSelectByName = mapMaybe (\n -> Map.lookup n nameToIdxMap) $ nodeNames impactOpts
+  let nodesList = nodesIdsSelectByName `union` (nodeIndexes impactOpts)
+  let shards = getNodesShardNum nodesList nodeIdxMap
+  let nodesSelectById = [AA.ShardID (AA.NodeID (Just n) Nothing Nothing) (fromIntegral s) | n <- nodesList
+                                                                                          , k <- shards
+                                                                                          , s <- take (fromIntegral k) [0..]]
+  let sets = shardSets `union` nodesSelectById
+  case null sets of
+    True -> errorWithoutStackTrace $ "Should specific shardSet using --shards, --nodeIndex or --nodeNames."
+    False -> sets
+
+buildCheckImpactRequest :: CheckImpactOpts -> AA.ShardSet -> AA.CheckImpactRequest
+buildCheckImpactRequest opts shardSet = AA.CheckImpactRequest
+  { checkImpactRequest_shards = shardSet
+  , checkImpactRequest_target_storage_state = Just (targetState opts)
+  , checkImpactRequest_disable_sequencers = Just (disableSequencers opts)
+  , checkImpactRequest_safety_margin = replicationProperty
+  , checkImpactRequest_log_ids_to_check = logIdsToCheck
+  , checkImpactRequest_abort_on_negative_impact = Just True
+  , checkImpactRequest_return_sample_size = Just 20
+  , checkImpactRequest_check_metadata_logs = Just (not $ skipMetaDataLogs opts)
+  , checkImpactRequest_check_internal_logs = Just (not $ skipInternalLogs opts)
+  , checkImpactRequest_check_capacity = Just (not skipCapacity)
+  , checkImpactRequest_max_unavailable_storage_capacity_pct = maxUnavailableStorageCapacity
+  , checkImpactRequest_max_unavailable_sequencing_capacity_pct = maxUnavailableSequencingCapacity
+  }
+  where
+    replicationProperty =
+      let margin = safetyMargin opts
+       in if null margin
+             then Nothing
+             else Just (Map.fromList $ map unReplicationPropertyPair margin)
+    logIdsToCheck =
+      let logIds = logs opts
+       in if null logIds
+             then Nothing
+             else Just logIds
+    skipCapacity = if skipCapacityChecks opts then True else False
+    maxUnavailableStorageCapacity = if skipCapacity then 100 else maxUnavailableStorageCapacityPct opts
+    maxUnavailableSequencingCapacity = if skipCapacity then 100 else maxUnavailableSequencingCapacityPct opts
+
+checkImpact :: AA.HeaderConfig AA.AdminAPI -> CheckImpactOpts -> IO ()
+checkImpact conf checkImpactOpts = do
+  nodeProperties <- getNodeProperties conf
+  let shardSets = combine checkImpactOpts nodeProperties
+  let checkImpactRequest = buildCheckImpactRequest checkImpactOpts shardSets
+  response <- AA.sendAdminApiRequest conf $ AA.checkImpact checkImpactRequest
+
+  let delta = fromJust $ AA.checkImpactResponse_total_duration response
+  let totalLogsChecked = fromJust $ AA.checkImpactResponse_total_logs_checked response
+  let responseImpact = AA.checkImpactResponse_impact response
+  case responseImpact of
+    [] -> putStrLn $ "ALL GOOD.\n Total logs checked (" <> show totalLogsChecked <> ") in " <> show delta <> "s"
+    impact -> putStrLn $ "UNSAFE. Impact: " <> (foldr (\x y -> show x <> ", " <> y) "" impact)
+              <> "\n Total logs checked (" <> show totalLogsChecked <> ") in " <> show delta <> "s"
+
