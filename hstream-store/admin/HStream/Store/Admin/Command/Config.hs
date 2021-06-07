982027806f5c9f2a00f1c100c90b830125de0c26
H⃝oo <56350037+Time-Hu@users.noreply.github.com>
2021-06-07 17:19:38 +0800

    add log rename (#231)

--- hstream-store/admin/HStream/Store/Admin/Command/Config.hs
+++ hstream-store/admin/HStream/Store/Admin/Command/Config.hs
@@ -7,6 +7,4 @@ import           Data.Text               (Text)
 import qualified HStream.Store.Admin.API as AA
 
 dumpConfig :: AA.HeaderConfig AA.AdminAPI ->  IO Text
-dumpConfig conf = do
-  config <- AA.sendAdminApiRequest conf $ AA.dumpServerConfigJson
-  return config
+dumpConfig conf = AA.sendAdminApiRequest conf AA.dumpServerConfigJson
