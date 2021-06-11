28656f4e1bfd00a3d049bb12e5128ca8fa377423
Mark Huang <8191686+wfnuser@users.noreply.github.com>
2021-06-11 17:17:21 +0800

    mysql connector (#257)
    * support mysql connector
    
    * start mysql server in ci

---
+++ hstream-connector/HStream/Connector/MySQL.hs
@@ -0,0 +1,46 @@
+{-# LANGUAGE BangPatterns      #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards   #-}
+
+module HStream.Connector.MySQL
+  ( mysqlSinkConnector
+  ) where
+
+import           Control.Monad                (void)
+import qualified Data.Aeson                   as Aeson
+import qualified Data.ByteString.Lazy.Char8   as DBCL
+import qualified Data.HashMap.Strict          as HM
+import           Data.List                    (intercalate)
+import           Data.Scientific              (floatingOrInteger)
+import qualified Data.Text                    as Text
+import           Database.MySQL.Base          as MySQL
+import           HStream.Utils                (flattenJSON)
+import qualified Z.IO.Logger                  as Log
+
+import           HStream.Processing.Connector (SinkConnector (..))
+import           HStream.Processing.Type      (SinkRecord (..))
+
+mysqlSinkConnector :: MySQL.MySQLConn -> SinkConnector
+mysqlSinkConnector myClient =
+  SinkConnector { writeRecord = writeRecordToMySQL myClient }
+
+toMySQLValue :: Aeson.Value -> String
+toMySQLValue (Aeson.String s) = "'" ++  show s ++ "'"
+toMySQLValue (Aeson.Bool b) = if b then "True" else "False"
+toMySQLValue (Aeson.Number sci) = do
+  case floatingOrInteger sci of
+    Left r  -> show (r :: Double)
+    Right i -> show (i :: Int)
+toMySQLValue _ = error "Not implemented"
+
+writeRecordToMySQL :: MySQL.MySQLConn -> SinkRecord -> IO ()
+writeRecordToMySQL myClient SinkRecord{..} = do
+  let insertMap = Aeson.decode snkValue :: Maybe (HM.HashMap Text.Text Aeson.Value)
+  case insertMap of
+    Just l -> do
+      let !flattened = flattenJSON l
+      let keys = "(" <> (intercalate "," . map Text.unpack $ HM.keys flattened) <> ")"
+          elems = "(" <> (intercalate "," . map toMySQLValue $ HM.elems flattened) <> ")"
+      void $ execute_ myClient $ Query $ DBCL.pack ("INSERT INTO " ++ Text.unpack snkStream ++ " " ++ keys ++ " VALUES " ++ elems)
+    _ -> do
+      Log.warning "Invalid Sink Value"
--- hstream/test/HStream/RunSQLSpec.hs
+++ hstream/test/HStream/RunSQLSpec.hs
@@ -1,17 +1,21 @@
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE PatternSynonyms   #-}
-{-# LANGUAGE RecordWildCards   #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE PatternSynonyms     #-}
+{-# LANGUAGE RecordWildCards     #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module HStream.RunSQLSpec (spec) where
 
 import           Control.Monad                    (replicateM)
 import qualified Data.ByteString.Char8            as C
+import qualified Data.ByteString.Lazy.Char8       as DBCL
 import           Database.ClickHouseDriver.Client
 import           Database.ClickHouseDriver.Types
+import qualified Database.MySQL.Base              as MySQL
 import           RIO
 import qualified RIO.ByteString.Lazy              as BL
 import qualified RIO.Map                          as Map
 import qualified RIO.Text                         as Text
+import           System.IO.Streams                as SIS hiding (take)
 import           System.IO.Unsafe                 (unsafePerformIO)
 import           System.Random
 import           Test.Hspec
@@ -19,6 +23,7 @@ import           Text.Printf                      (printf)
 
 import           HStream.Connector.ClickHouse
 import           HStream.Connector.HStore
+import           HStream.Connector.MySQL
 import           HStream.Processing.Connector
 import           HStream.Processing.Processor
 import           HStream.Processing.Type
@@ -39,16 +44,17 @@ spec = describe "HStream.RunSQLSpec" $ do
   source2 <- runIO $ newRandomText 10
   sink1 <- runIO $ newRandomText 10
   sink2 <- runIO $ newRandomText 10
-
-  let source1 = "source3"
+  let source3 = "source3"
 
   it "create streams" $
     (do
         setLogDeviceDbgLevel C_DBG_ERROR
         handleDropStreamSQL $ "DROP STREAM IF EXIST " <> source1 <> " ;"
         handleDropStreamSQL $ "DROP STREAM IF EXIST " <> source2 <> " ;"
+        handleDropStreamSQL $ "DROP STREAM IF EXIST " <> source3 <> " ;"
         handleCreateStreamSQL $ "CREATE STREAM " <> source1 <> " WITH (REPLICATE = 3);"
         handleCreateStreamSQL $ "CREATE STREAM " <> source2 <> ";"
+        handleCreateStreamSQL $ "CREATE STREAM " <> source3 <> " WITH (REPLICATE = 3);"
         handleCreateStreamSQL $ "CREATE STREAM " <> sink1   <> " WITH (REPLICATE = 3);"
         handleCreateStreamSQL $ "CREATE STREAM " <> sink2   <> " ;"
     ) `shouldReturn` ()
@@ -62,12 +68,14 @@ spec = describe "HStream.RunSQLSpec" $ do
 
   it "create connectors" $
     (do
-      handleCreateConnectorSQL $ "CREATE SOURCE | SINK CONNECTOR clickhouse1 WITH (type = \"clickhouse\", streamname = \""<> source1 <>"\");"
-      handleInsertSQL $ "INSERT INTO " <> source1 <> " (temperature, humidity) VALUES (12, 84);"
-      handleInsertSQL $ "INSERT INTO " <> source1 <> " (temperature, humidity) VALUES (22, 83);"
-      handleInsertSQL $ "INSERT INTO " <> source1 <> " (temperature, humidity) VALUES (32, 82);"
-      handleInsertSQL $ "INSERT INTO " <> source1 <> " (temperature, humidity) VALUES (42, 81);"
+      handleCreateMysqlTable source3
+      handleCreateConnectorSQL $ "CREATE SOURCE | SINK CONNECTOR mysql WITH (type = \"mysql\", host = \"127.0.0.1\", streamname = \""<> source3 <>"\");"
+      handleInsertSQL $ "INSERT INTO " <> source3 <> " (temperature, humidity) VALUES (12, 84);"
+      handleInsertSQL $ "INSERT INTO " <> source3 <> " (temperature, humidity) VALUES (22, 83);"
+      handleInsertSQL $ "INSERT INTO " <> source3 <> " (temperature, humidity) VALUES (32, 82);"
+      handleInsertSQL $ "INSERT INTO " <> source3 <> " (temperature, humidity) VALUES (42, 81);"
       threadDelay 5000000
+      handleCleanMysql source3
     ) `shouldReturn` ()
 
   it "drop streams" $
@@ -130,34 +138,45 @@ handleCreateConnectorSQL sql = do
           let sc = hstoreSourceConnector ldclient ldreader
           let streamM = lookup "streamname" cOptions
           let typeM = lookup "type" cOptions
-          let resp = genSuccessQueryResponse
           let fromCOptionString m = case m of
                 Just (ConstantString s) -> Just $ C.pack s
                 _                       -> Nothing
-          let sk = case typeM of
+          let fromCOptionStringToString m = case m of
+                Just (ConstantString s) -> Just s
+                _                       -> Nothing
+          let fromCOptionIntToPortNumber m = case m of
+                Just (ConstantInt s) -> Just $ fromIntegral s
+                _                    -> Nothing
+          let sk' = case typeM of
                 Just (ConstantString cType) ->
                   do
                     case cType of
                         "clickhouse" -> do
-                          let username = fromMaybe "default" $ fromCOptionString (lookup "username" cOptions)
-                          let host = fromMaybe "127.0.0.1" $ fromCOptionString (lookup "host" cOptions)
-                          let port = fromMaybe "9000" $ fromCOptionString (lookup "port" cOptions)
-                          let password = fromMaybe "" $ fromCOptionString (lookup "password" cOptions)
-                          let database = fromMaybe "default" $ fromCOptionString (lookup "database" cOptions)
-                          let cli = clickHouseSinkConnector $ createClient ConnParams{
-                              username'     = username
-                              ,host'        = host
-                              ,port'        = port
-                              ,password'    = password
-                              ,compression' = False
-                              ,database'    = database
-                          }
-                          Right cli
-                        _ -> Left "unsupported sink connector"
-                _ -> Left "Invalid type in connector options"
+                          cli <- createClient $ ConnParams
+                              (fromMaybe "default" $ fromCOptionString (lookup "username" cOptions))
+                              (fromMaybe "127.0.0.1" $ fromCOptionString (lookup "host" cOptions))
+                              (fromMaybe "9000" $ fromCOptionString (lookup "port" cOptions))
+                              (fromMaybe "" $ fromCOptionString (lookup "password" cOptions))
+                              False
+                              (fromMaybe "default" $ fromCOptionString (lookup "database" cOptions))
+                          let connector = clickHouseSinkConnector cli
+                          return $ Right connector
+                        "mysql" -> do
+                          conn <- MySQL.connect $ MySQL.ConnectInfo
+                              (fromMaybe "127.0.0.1" $ fromCOptionStringToString (lookup "host" cOptions))
+                              (fromMaybe 3306 $ fromCOptionIntToPortNumber (lookup "port" cOptions))
+                              (fromMaybe "mysql" $ fromCOptionString (lookup "database" cOptions))
+                              (fromMaybe "root" $ fromCOptionString (lookup "username" cOptions))
+                              (fromMaybe "password" $ fromCOptionString (lookup "password" cOptions))
+                              33
+                          let connector = mysqlSinkConnector conn
+                          return $ Right connector
+                        _ -> return $ Left "unsupported sink connector"
+                _ -> return $ Left "Invalid type in connector options"
+          sk <- sk'
           case sk of
             Left err -> error err
-            Right sk -> do
+            Right conn -> do
               case streamM of
                 Just (ConstantString stream) -> do
                   subscribeToStream sc (Text.pack stream) Latest
@@ -167,7 +186,7 @@ handleCreateConnectorSQL sql = do
                           records <- readRecords sc
                           forM_ records $ \SourceRecord {..} ->
                             writeRecord
-                              sk
+                              conn
                               SinkRecord
                                 { snkStream = (Text.pack stream),
                                   snkKey = srcKey,
@@ -224,3 +243,41 @@ handleCreateBySelectSQL sql = do
 
 newRandomText :: Int -> IO Text
 newRandomText n = Text.pack . take n . randomRs ('a', 'z') <$> newStdGen
+
+-- create mysql table for testing
+handleCreateMysqlTable :: Text -> IO ()
+handleCreateMysqlTable source = do
+  conn <- MySQL.connect MySQL.ConnectInfo {
+    ciUser = "root",
+    ciPassword = "password",
+    ciPort = 3306,
+    ciHost = "127.0.0.1",
+    ciDatabase = "mysql",
+    ciCharset = 33
+  }
+
+  _ <- MySQL.execute_ conn $ MySQL.Query . DBCL.pack $ "CREATE TABLE IF NOT EXISTS "<> Text.unpack source <>"(temperature INT, humidity INT) CHARACTER SET utf8"
+  MySQL.close conn
+
+-- clean mysql data and check insert rows are correct
+handleCleanMysql :: Text -> IO ()
+handleCleanMysql source = do
+  conn <- MySQL.connect MySQL.ConnectInfo {
+    ciUser = "root",
+    ciPassword = "password",
+    ciPort = 3306,
+    ciHost = "127.0.0.1",
+    ciDatabase = "mysql",
+    ciCharset = 33
+  }
+
+  (_, is) <- MySQL.query_ conn $ MySQL.Query . DBCL.pack $ "SELECT * FROM " <> Text.unpack source
+  (rowCount :: Int) <- SIS.fold (\s _ -> s+1) 0 is
+
+  _ <- MySQL.execute_ conn $ MySQL.Query . DBCL.pack $ "DROP TABLE IF EXISTS " <> Text.unpack source
+
+  MySQL.close conn
+
+  if rowCount == 4
+    then return ()
+    else error "Sink connector sync data failed"
