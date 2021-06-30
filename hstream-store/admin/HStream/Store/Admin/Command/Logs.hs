7a49707a9c3bffb40f9d01544e3d518e421bb706
mu <59917266+4eUeP@users.noreply.github.com>
2021-06-30 16:58:47 +0800

    hadmin: show numeric lsn for logs info

--- hstream-store/admin/HStream/Store/Admin/Command/Logs.hs
+++ hstream-store/admin/HStream/Store/Admin/Command/Logs.hs
@@ -73,33 +73,36 @@ runLogsInfo conf logid = do
     info client = do
       putStrLn "Tail Info:"
       lsn <- S.getTailLSN client logid
-      putStrLn $ "  Tail lsn: " ++ prettyPrintLSN lsn
+      putStrLn $ "  Tail lsn: " <> prettyPrintLSN lsn <> " (" <> show lsn <> ")"
       tail_attr <- S.getLogTailAttrs client logid
       tail_lsn <- S.getLogTailAttrsLSN tail_attr
       putStrLn $ "  Last released real (record) lsn: "
-        ++ prettyPrintLSN tail_lsn
+              <> prettyPrintLSN tail_lsn <> " (" <> show tail_lsn <> ")"
       tail_time_stamp <- S.getLogTailAttrsLastTimeStamp tail_attr
       putStrLn $ "  Approximate timestamp of last released real lsn: "
-        ++ prettyPrintTimeStamp tail_time_stamp
+              <> prettyPrintTimeStamp tail_time_stamp
       tail_offset <- S.getLogTailAttrsBytesOffset tail_attr
       putStrLn $ "  Approximate byte offset of the tail of the log: "
-        ++ show tail_offset
+              <> show tail_offset
 
       putStrLn "Head Info:"
       head_attr <- S.getLogHeadAttrs client logid
       head_lsn <- S.getLogHeadAttrsTrimPoint head_attr
-      putStrLn $ "  Trim point: " ++ prettyPrintLSN head_lsn
+      putStrLn $ "  Trim point: "
+              <> prettyPrintLSN head_lsn <> "(" <> show head_lsn <> ")"
       head_time_stamp <- S.getLogHeadAttrsTrimPointTimestamp head_attr
       putStrLn $ "  Approximate timestamp of trim point (ms): "
-        ++ prettyPrintTimeStamp head_time_stamp
+              <> prettyPrintTimeStamp head_time_stamp
       empty <- S.isLogEmpty client logid
       putStrLn $ "Log Empty?: " <> show empty
 
     prettyPrintLSN lsn =
       "e" ++ show (lsn `shiftR` 32) ++ "n" ++ show (lsn .&. 0xFFFFFFFF)
+
     prettyPrintTimeStamp ts = unsafePerformIO $ do
-      time <- formatSystemTime simpleDateFormat (MkSystemTime (ts `div` 1000) 0)
-      return $ show ts ++ " -> " ++ show time
+      time_str <- if ts /= maxBound then show <$> formatSystemTime simpleDateFormat (MkSystemTime (ts `div` 1000) 0)
+                                    else return "INVALID!"
+      return $ show ts ++ " -> " ++ time_str
 
 runLogsRename :: HeaderConfig AdminAPI -> CBytes -> CBytes -> Bool -> IO ()
 runLogsRename conf old new warn = do
