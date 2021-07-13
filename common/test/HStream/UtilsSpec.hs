9ed553b0bc5d55ec76fff186710eea25ef3e47df
mu <59917266+4eUeP@users.noreply.github.com>
2021-07-13 15:40:35 +0800

    add hstream-common-test

---
+++ common/test/HStream/UtilsSpec.hs
@@ -0,0 +1,20 @@
+module HStream.UtilsSpec (spec) where
+
+import           Control.Concurrent
+import           Control.Monad
+import qualified Data.Set           as Set
+import           HStream.Utils
+import           Test.Hspec
+
+spec :: Spec
+spec = describe "HStream.Utils" $ do
+
+  it "genUniqueSpec" $ do
+    let maxInflights = 5
+        eachTimes = 5
+    results <- forM [0..maxInflights-1] $ const newEmptyMVar
+    forM_ [0..maxInflights-1] $ \idx -> forkIO $ do
+      r <- replicateM eachTimes genUnique
+      putMVar (results !! idx) r
+    rs <- concat <$> forM [0..maxInflights-1] (takeMVar . (results !!))
+    length rs `shouldBe` Set.size (Set.fromList rs)
