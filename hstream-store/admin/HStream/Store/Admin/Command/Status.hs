2e006d246693a9123653a79476c3f20534180881
mu <59917266+4eUeP@users.noreply.github.com>
2021-07-16 11:16:00 +0800

    hadmin status: more infos

--- hstream-store/admin/HStream/Store/Admin/Command/Status.hs
+++ hstream-store/admin/HStream/Store/Admin/Command/Status.hs
@@ -3,13 +3,46 @@ module HStream.Store.Admin.Command.Status
   ) where
 
 import           Control.Monad
+import           Data.Int                   (Int64)
 import           Data.List                  (elemIndex, sortBy)
+import           Data.Maybe                 (fromJust)
 import qualified Data.Text                  as Text
+import qualified Data.Text.Encoding         as Text
+import           Data.Time.Clock.POSIX      (POSIXTime, getPOSIXTime)
 import qualified Text.Layout.Table          as Table
 
 import qualified HStream.Store.Admin.API    as AA
 import           HStream.Store.Admin.Format (simpleShowTable)
 import           HStream.Store.Admin.Types
+import           HStream.Utils              (approxNaturaltime)
+
+data NodeState' = NodeState'
+  { stateState      :: AA.NodeState
+  , stateVersion    :: Text.Text
+  , stateAliveSince :: Int64
+  }
+
+showID :: NodeState' -> String
+showID = show . AA.nodeConfig_node_index . AA.nodeState_config . stateState
+
+showName :: NodeState' -> String
+showName = Text.unpack . AA.nodeConfig_name . AA.nodeState_config . stateState
+
+showDaemonState :: NodeState' -> String
+showDaemonState = cutLast' "_" . show . AA.nodeState_daemon_state . stateState
+
+showHealthState :: NodeState' -> String
+showHealthState = cutLast' "_" . show . AA.nodeState_daemon_health_status . stateState
+
+showVersion :: NodeState' -> String
+showVersion = Text.unpack . stateVersion
+
+showUptime :: POSIXTime -> NodeState' -> String
+showUptime time state =
+  approxNaturaltime (time - fromIntegral (stateAliveSince state)) ++ " ago"
+
+showSeqState :: NodeState' -> String
+showSeqState = cutLast' "_" . maybe " " (show . AA.sequencerState_state) . AA.nodeState_sequencer_state . stateState
 
 -- | Gets the state object for all nodes that matches the supplied NodesFilter.
 --
@@ -27,19 +60,48 @@ runStatus conf StatusOpts{..} = do
         rs <- forM xs $ \x -> AA.nodesStateResponse_states <$> AA.getNodesState (AA.NodesStateRequest (Just x) (Just statusForce))
         return $ concat rs
 
-  let titles = ["ID", "NAME", "STATE", "HEALTH STATUS"]
-  let getID = show . AA.nodeConfig_node_index . AA.nodeState_config
-  let getName = Text.unpack . AA.nodeConfig_name . AA.nodeState_config
-  let getState = Text.unpack . last . Text.splitOn "_" . Text.pack . show . AA.nodeState_daemon_state
-  let getHealthState = Text.unpack . last . Text.splitOn "_" . Text.pack . show . AA.nodeState_daemon_health_status
-  let collectState s = map ($ s) [getID, getName, getState, getHealthState]
-  let allStates = map collectState states
+  let getNodeHeaderConfig sa =
+        AA.HeaderConfig
+          { AA.headerHost = Text.encodeUtf8 . fromJust $ AA.socketAddress_address sa
+          , AA.headerPort = fromIntegral . fromJust $ AA.socketAddress_port sa
+          , AA.headerProtocolId  = AA.binaryProtocolId
+          , AA.headerConnTimeout = 5000
+          , AA.headerSendTimeout = 5000
+          , AA.headerRecvTimeout = 5000
+          }
+  additionStates <- forM states $ \state -> do
+    let hc = getNodeHeaderConfig . AA.getNodeAdminAddr $ AA.nodeState_config state
+    AA.sendAdminApiRequest hc $ do
+      version <- AA.getVersion
+      aliveSince <- AA.aliveSince
+      return (version, aliveSince)
+  let allStates = zipWith (\state (version, alive) -> NodeState' state version alive) states additionStates
+  currentTime <- getPOSIXTime
+
+  let cons = [ ("ID", showID)
+             , ("NAME", showName)
+             , ("PACKAGE", showVersion)
+             , ("STATE", showDaemonState)
+             , ("UPTIME", showUptime currentTime)
+             , ("SEQ.", showSeqState)
+             , ("HEALTH STATUS", showHealthState)
+             ]
+  let titles = map fst cons
+      collectedState = map (\s -> map (($ s) . snd) cons) allStates
 
   let m_sortIdx = elemIndex (Text.unpack . Text.toUpper $ statusSortField) titles
   case m_sortIdx of
     Just sortIdx -> do
-      let stats = sortBy (\xs ys -> compare (xs!!sortIdx) (ys!!sortIdx)) allStates
+      let stats = sortBy (\xs ys -> compare (xs!!sortIdx) (ys!!sortIdx)) collectedState
       case statusFormat of
         TabularFormat -> return $ simpleShowTable (map (, 20, Table.left) titles) stats
         JSONFormat    -> errorWithoutStackTrace "NotImplemented"
     Nothing -> errorWithoutStackTrace $ "No such sort key: " <> Text.unpack statusSortField
+
+-------------------------------------------------------------------------------
+
+cutLast :: Text.Text -> Text.Text -> Text.Text
+cutLast splitor = last . Text.splitOn splitor
+
+cutLast' :: Text.Text -> String -> String
+cutLast' splitor = Text.unpack . cutLast splitor . Text.pack
