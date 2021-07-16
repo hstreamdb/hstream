a9fc9ba01c3893b61350564834451c712ee1f3d0
mu <59917266+4eUeP@users.noreply.github.com>
2021-07-16 15:40:48 +0800

    hadmin: handle StoreException while running sql (#437)

--- hstream-store/admin/HStream/Store/Admin/Format.hs
+++ hstream-store/admin/HStream/Store/Admin/Format.hs
@@ -1,9 +1,14 @@
 module HStream.Store.Admin.Format
   ( simpleShowTable
+  , handleStoreError
   ) where
 
-import           Data.Default      (def)
-import qualified Text.Layout.Table as Table
+import qualified Control.Exception               as E
+import           Data.Default                    (def)
+import qualified HStream.Store                   as S
+import qualified Options.Applicative.Help.Pretty as P
+import qualified Text.Layout.Table               as Table
+import qualified Z.Data.Text                     as T
 
 simpleShowTable :: [(String, Int, Table.Position Table.H)] -> [[String]] -> String
 simpleShowTable _ [] = ""
@@ -14,3 +19,10 @@ simpleShowTable colconfs rols =
                         Table.asciiS
                         (Table.titlesH titles)
                         [ Table.rowsG rols ]
+
+handleStoreError :: IO () -> IO ()
+handleStoreError action =
+  let putErr = P.putDoc . P.red . P.string . (\s -> "Error: " <> s <> "\n") .  T.toString . S.sseDescription
+   in action `E.catches` [ E.Handler (\(S.StoreError ex) -> putErr ex)
+                         , E.Handler (\(ex :: S.SomeHStoreException) -> print ex)
+                         ]
