f27504f64bff68bbe0456e8d3e36b760cdc7acf8
mu <59917266+4eUeP@users.noreply.github.com>
2021-07-21 11:47:09 +0800

    code refactroring (#443)
    * dev-tools: add start/stop services command
    * add an echo rpc for testing
    * improvements for tests

--- hstream/test/HStream/RunConnectorSpec.hs
+++ hstream/test/HStream/RunConnectorSpec.hs
@@ -8,6 +8,7 @@ module HStream.RunConnectorSpec (spec) where
 
 import qualified Data.List                        as L
 import qualified Data.Map.Strict                  as Map
+import           Data.Maybe                       (isJust)
 import qualified Data.Text.Lazy                   as TL
 import qualified Data.Vector                      as V
 import           Network.GRPC.HighLevel.Generated
@@ -20,13 +21,15 @@ import           HStream.Store.Logger
 getConnectorResponseIdIs :: TL.Text -> Connector -> Bool
 getConnectorResponseIdIs targetId (Connector connectorId _ _ _ ) = connectorId == targetId
 
+-- TODO: mv to hstream-client library
+
 createSinkConnector :: TL.Text -> IO (Maybe Connector)
 createSinkConnector sql = withGRPCClient clientConfig $ \client -> do
   HStreamApi{..} <- hstreamApiClient client
   let createSinkConnectorRequest = CreateSinkConnectorRequest { createSinkConnectorRequestSql = sql }
   resp <- hstreamApiCreateSinkConnector (ClientNormalRequest createSinkConnectorRequest 100 (MetadataMap Map.empty))
   case resp of
-    ClientNormalResponse x@Connector{} _meta1 _meta2 StatusOk _details -> return $ Just x
+    ClientNormalResponse x _meta1 _meta2 StatusOk _details -> return $ Just x
     ClientErrorResponse clientError -> do
       putStrLn $ "Create Connector Client Error: " <> show clientError
       return Nothing
@@ -36,7 +39,7 @@ listConnectors :: IO (Maybe ListConnectorsResponse)
 listConnectors = withGRPCClient clientConfig $ \client -> do
   HStreamApi{..} <- hstreamApiClient client
   let listConnectorsRequest = ListConnectorsRequest {}
-  resp <- hstreamApiListConnectors (ClientNormalRequest listConnectorsRequest 100 (MetadataMap $ Map.empty))
+  resp <- hstreamApiListConnectors (ClientNormalRequest listConnectorsRequest 100 (MetadataMap Map.empty))
   case resp of
     ClientNormalResponse x@ListConnectorsResponse{} _meta1 _meta2 _status _details -> return $ Just x
     ClientErrorResponse clientError -> do
@@ -94,45 +97,25 @@ restartConnector connectorId = withGRPCClient clientConfig $ \client -> do
 spec :: Spec
 spec = describe "HStream.RunConnectorSpec" $ do
   source1 <- runIO $ TL.fromStrict <$> newRandomText 20
+  runIO $ setLogDeviceDbgLevel C_DBG_ERROR
   let mysqlConnector = "mysql"
 
-  it "clean streams" $
-    ( do
-        setLogDeviceDbgLevel C_DBG_ERROR
-        res1 <- executeCommandQuery $ "DROP STREAM " <> source1 <> " IF EXISTS ;"
-        return [res1]
-    ) `shouldReturn` L.replicate 1 (Just successResp)
+  it "create mysql sink connector" $ do
+    executeCommandQuery' ("DROP STREAM " <> source1 <> " IF EXISTS ;")
+      `shouldReturn` successResp
+    executeCommandQuery' ("CREATE STREAM " <> source1 <> " WITH (REPLICATE = 3);")
+      `shouldReturn` successResp
 
-  it "create streams" $
-    ( do
-        res1 <- executeCommandQuery $ "CREATE STREAM " <> source1 <> " WITH (REPLICATE = 3);"
-        return [res1]
-    ) `shouldReturn` L.replicate 1 (Just successResp)
+    createSinkConnector (createMySqlConnectorSql mysqlConnector source1)
+      >>= (`shouldSatisfy` isJust)
 
-  it "create mysql sink connector" $
-    ( do
-        res <- createSinkConnector ("CREATE SINK CONNECTOR " <> mysqlConnector <> " WITH (type = mysql, host = \"127.0.0.1\", stream = " <> source1 <> ");")
-        case res of
-          Just _ -> return True
-          _      -> return False
-    ) `shouldReturn` True
+  it "list connectors" $ do
+    Just ListConnectorsResponse {listConnectorsResponseConnectors = connectors} <- listConnectors
+    let record = V.find (getConnectorResponseIdIs mysqlConnector) connectors
+    record `shouldSatisfy` isJust
 
-  it "list connectors" $
-    ( do
-        Just ListConnectorsResponse {listConnectorsResponseConnectors = connectors} <- listConnectors
-        let record = V.find (getConnectorResponseIdIs mysqlConnector) connectors
-        case record of
-          Just _ -> return True
-          _      -> return False
-    ) `shouldReturn` True
-
-  it "get connector" $
-    ( do
-        connector <- getConnector mysqlConnector
-        case connector of
-          Just _ -> return True
-          _      -> return False
-    ) `shouldReturn` True
+  it "get connector" $ do
+    getConnector mysqlConnector >>= (`shouldSatisfy` isJust)
 
   it "cancel connector" $
     ( do
