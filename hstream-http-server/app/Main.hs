ec72c361958c6f9bbe6b18d186cdc3ac842194e9
mu <59917266+4eUeP@users.noreply.github.com>
2021-08-05 10:57:49 +0800

    http sever: fix no effect with --host param

--- hstream-http-server/app/Main.hs
+++ hstream-http-server/app/Main.hs
@@ -4,8 +4,10 @@
 
 module Main where
 
+import           Data.String                      (fromString)
 import           Network.GRPC.HighLevel.Generated
-import           Network.Wai.Handler.Warp         (run)
+import           Network.Wai.Handler.Warp         (defaultSettings, runSettings,
+                                                   setHost, setPort)
 import           Options.Applicative              (Parser, auto, execParser,
                                                    fullDesc, help, helper, info,
                                                    long, metavar, option,
@@ -15,25 +17,30 @@ import           Servant.Server                   (serve)
 
 import           HStream.HTTP.Server.API          (ServerConfig (..), api,
                                                    apiServer)
+import qualified HStream.Logger                   as Log
 
 parseConfig :: Parser ServerConfig
 parseConfig =
   ServerConfig
-    <$> strOption   (long "host"                 <> metavar "HOST" <> showDefault <> value "127.0.0.1"                  <> help "server host value")
-    <*> option auto (long "port"                 <> metavar "INT"  <> showDefault <> value 8000 <> short 'p'            <> help "server port value")
-    <*> strOption   (long "hstream-host"         <> metavar "HOST" <> showDefault <> value "127.0.0.1"                  <> help "hstream grpc server host value")
-    <*> option auto (long "hstream-port"         <> metavar "INT"  <> showDefault <> value 6570                         <> help "hstream grpc server port value")
+    <$> strOption   (long "host"         <> metavar "HOST" <> showDefault <> value "127.0.0.1"       <> help "server host value")
+    <*> option auto (long "port"         <> metavar "INT"  <> showDefault <> value 8000 <> short 'p' <> help "server port value")
+    <*> strOption   (long "hstream-host" <> metavar "HOST" <> showDefault <> value "127.0.0.1"       <> help "hstream grpc server host value")
+    <*> option auto (long "hstream-port" <> metavar "INT"  <> showDefault <> value 6570              <> help "hstream grpc server port value")
 
 app :: ServerConfig -> IO ()
 app ServerConfig{..} = do
   let clientConfig = ClientConfig { clientServerHost = Host _hstreamHost
-                            , clientServerPort = Port _hstreamPort
-                            , clientArgs = []
-                            , clientSSLConfig = Nothing
-                            , clientAuthority = Nothing
-                            }
-  withGRPCClient clientConfig $
-    \hClient -> run _serverPort $ serve api $ apiServer hClient
+                                  , clientServerPort = Port _hstreamPort
+                                  , clientArgs = []
+                                  , clientSSLConfig = Nothing
+                                  , clientAuthority = Nothing
+                                  }
+      serverConfig = setPort _serverPort . setHost (fromString _serverHost) $ defaultSettings
+  withGRPCClient clientConfig $ \hClient -> do
+    Log.info $ "Starting HStream http server on "
+            <> Log.buildString _serverHost <> ":"
+            <> Log.buildInt _serverPort
+    runSettings serverConfig $ serve api $ apiServer hClient
 
 main :: IO ()
 main = do
