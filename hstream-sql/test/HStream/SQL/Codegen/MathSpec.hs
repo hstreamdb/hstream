f0117b9cd215d314a8118303998641264b11dcfc
alissa-tung <alissa-tung@outlook.com>
2021-08-11 17:10:06 +0800

    more tests

--- hstream-sql/test/HStream/SQL/Codegen/MathSpec.hs
+++ hstream-sql/test/HStream/SQL/Codegen/MathSpec.hs
@@ -1,16 +1,29 @@
-{-# LANGUAGE OverloadedLists   #-}
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE BlockArguments      #-}
+{-# LANGUAGE OverloadedLists     #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module HStream.SQL.Codegen.MathSpec where
 
 import qualified Data.Aeson                   as A
 import           Data.Function
+import           Data.Scientific
 import           HStream.SQL.AST
 import           HStream.SQL.Internal.Codegen
+import           Test.HUnit
 import           Test.Hspec
 
 spec :: Spec
-spec = describe "Math Scalar Functions" $ do
+spec = describe "Math Scalar Functions" do
 
-  it "binary add" $ do
+-- Arith Expr
+
+  it "binary add" do
     binOpOnValue OpAdd (A.Number 1) (A.Number 2) `shouldBe` (A.Number 3)
+
+  it "floor" do
+    unaryOpOnValue OpFloor (A.Number 1.5)
+      `shouldBe` A.Number 1
+
+  it "ceil" do
+    unaryOpOnValue OpCeil (A.Number 1.5) `shouldBe` A.Number 2
--- hstream-sql/test/HStream/SQL/ValidateSpec.hs
+++ hstream-sql/test/HStream/SQL/ValidateSpec.hs
@@ -14,3 +14,8 @@ spec = describe "Validate Basic Data Types" $ do
     validate (DDate (Nothing :: BNFC'Position)
               (IPInteger Nothing 2021) (IPInteger Nothing 02) (IPInteger Nothing 29))
       `shouldSatisfy` isLeft
+
+  it "time" $ do
+    validate (DTime (Nothing :: BNFC'Position)
+      (IPInteger Nothing 14) (IPInteger Nothing 61) (IPInteger Nothing 59))
+        `shouldSatisfy` isLeft
