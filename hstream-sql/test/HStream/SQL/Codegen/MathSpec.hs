a9215a3114a94ae9302cfb4b0b8db6d0d9a3b6f3
alissa-tung <alissa-tung@outlook.com>
2021-08-11 14:39:34 +0800

    sql: add array function tests

--- hstream-sql/test/HStream/SQL/Codegen/MathSpec.hs
+++ hstream-sql/test/HStream/SQL/Codegen/MathSpec.hs
@@ -1,14 +1,16 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedLists     #-}
+{-# LANGUAGE OverloadedStrings   #-}
 
-module HStream.SQL.CodegenSpec where
+module HStream.SQL.Codegen.MathSpec where
 
 import qualified Data.Aeson                   as A
+import           Data.Function
 import           HStream.SQL.AST
 import           HStream.SQL.Internal.Codegen
 import           Test.Hspec
 
 spec :: Spec
-spec = describe "Scalar Functions" $ do
+spec = describe "Math Scalar Functions" $ do
 
   it "binary add" $ do
     binOpOnValue OpAdd (A.Number 1) (A.Number 2) `shouldBe` (A.Number 3)
