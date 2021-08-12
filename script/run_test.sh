3c76d246e9e608f31e5df4dec026c884f9069073
mu <59917266+4eUeP@users.noreply.github.com>
2021-08-12 10:00:33 +0800

    remove unused run_test script

--- script/run_test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-GHC=${GHC:-latest}
-COMPONENT="$1"
-
-echo "====== Test $COMPONENT ======"
-python3 script/dev-tools cabal --check --no-interactive -i docker.io/hstreamdb/haskell:$GHC \
-    -- test --test-show-details=direct $COMPONENT
