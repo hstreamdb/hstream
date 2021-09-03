d82bc343941bc61c034c050e48d601dd8cb8981d
mu <59917266+4eUeP@users.noreply.github.com>
2021-09-03 17:28:55 +0800

    Stats: simple counter benchmark (#550)

--- common/bench/stats.hs
+++ common/bench/stats.hs
@@ -2,16 +2,35 @@
 
 module Main where
 
-import           Control.Concurrent
 import           Control.Monad
 import           Criterion.Main
+import           Data.IORef
 
 import           HStream.Stats
-
--- TODO
+import           HStream.Utils  (runConc)
 
 -- | To run the benchmark:
 --
--- > cabal run -- common-bench-stats --output bench.html --regress allocated:iters +RTS -T
+-- > cabal run -- common-bench-stats --output bench.html --regress allocated:iters --regress numGcs:iters +RTS -T
 main :: IO ()
-main = undefined
+main =
+  defaultMain
+    [ bgroup "counter" [ bench "IORef" $ nfIO (runIORef 100000)
+                       , bench "StatsHolder" $ nfIO (runStatsHolder 100000)
+                       ]
+    ]
+
+runIORef :: Int -> IO ()
+runIORef n = do
+  ref <- newIORef 0
+  runConc n $ atomicModifyIORef' ref (\x -> (x+1, ()))
+  r <- readIORef ref
+  when (r /= n) $ error "Error: wrong result!"
+
+runStatsHolder :: Int -> IO ()
+runStatsHolder n = do
+  h <- newStatsHolder
+  runConc n $ stream_stat_add_append_payload_bytes h "name" 1
+  s <- newAggregateStats h
+  r <- fromIntegral <$> stream_stat_get_append_payload_bytes s "name"
+  when (r /= n) $ error "Error: wrong result!"
