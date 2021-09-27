e1dc5bc91c10a07c563900e9dc7925408edb27d2
mu <59917266+4eUeP@users.noreply.github.com>
2021-09-27 11:27:43 +0800

    Stats: add stream_time_series_get for a specific stream

--- common/cbits/hs_stats.cpp
+++ common/cbits/hs_stats.cpp
@@ -151,6 +151,85 @@ void stream_time_series_getall_by_name(
       output, nullptr, nullptr, len, keys_ptr, values_ptr, keys_, values_);
 }
 
+int stream_time_series_get(StatsHolder* stats_holder, const char* string_name,
+                           const char* stream_name_,
+                           //
+                           HsInt interval_size, HsInt* ms_intervals,
+                           HsDouble* aggregate_vals) {
+  using Duration = PerStreamTimeSeries::Duration;
+  using TimePoint = PerStreamTimeSeries::TimePoint;
+
+  std::shared_ptr<PerStreamTimeSeries> PerStreamStats::*member_ptr = nullptr;
+#define TIME_SERIES_DEFINE(name, strings, _, __)                               \
+  for (const std::string& str : strings) {                                     \
+    if (str == std::string(string_name)) {                                     \
+      member_ptr = &PerStreamStats::name;                                      \
+      break;                                                                   \
+    }                                                                          \
+  }
+#include "per_stream_time_series.inc"
+  // TODO: also return failure reasons
+  if (UNLIKELY(member_ptr == nullptr)) {
+    return -1;
+  }
+
+  bool has_found = false;
+  stats_holder->runForEach([&](Stats& s) {
+    // Use synchronizedCopy() so we do not have to hold a read lock on
+    // per_log_stats map while we iterate over individual entries.
+    for (auto& entry : s.synchronizedCopy(&Stats::per_stream_stats)) {
+      std::lock_guard<std::mutex> guard(entry.second->mutex);
+
+      std::string& stream_name = entry.first;
+      auto time_series = entry.second.get()->*member_ptr;
+      if (!time_series) {
+        continue;
+      }
+
+      if (stream_name == std::string(stream_name_)) {
+        // NOTE: It might be tempting to pull `now' out of the loops but
+        // folly::MultiLevelTimeSeries barfs if we ask it for data that is
+        // too old.  Keep it under the lock for now, optimize if necessary.
+        //
+        // TODO: Constructing the TimePoint is slightly awkward at the moment
+        // as the folly stats code is being cleaned up to better support real
+        // clock types.  appendBytesTimeSeries_ should simply be changed to
+        // use std::steady_clock as it's clock type.  I'll do that in a
+        // separate diff for now, though.
+        const TimePoint now{std::chrono::duration_cast<Duration>(
+            std::chrono::steady_clock::now().time_since_epoch())};
+        // Flush any cached updates and discard any stale data
+        time_series->update(now);
+
+        // For each query interval, make a MultiLevelTimeSeries::rate() call
+        // to find the approximate rate over that interval
+        for (int i = 0; i < interval_size; ++i) {
+          const Duration interval = std::chrono::duration_cast<Duration>(
+              std::chrono::milliseconds{ms_intervals[i]});
+
+          auto rate_per_time_type =
+              time_series->rate<double>(now - interval, now);
+          // Duration may not be seconds, convert to seconds
+          aggregate_vals[i] += rate_per_time_type * Duration::period::den /
+                               Duration::period::num;
+        }
+
+        // We have aggregated the stat from this Stats, because stream name
+        // in Stats are unique(as keys of unordered_map), we can break the loop
+        // safely.
+        if (!has_found)
+          has_found = true;
+        break;
+      }
+    }
+  });
+
+  if (has_found)
+    return 0;
+  else
+    return -1;
+}
+
 // TODO
 // bool verifyIntervals(StatsHolder* stats_holder, std::string string_name,
 //                     std::vector<Duration> query_intervals, std::string& err)
--- common/test/HStream/StatsSpec.hs
+++ common/test/HStream/StatsSpec.hs
@@ -31,6 +31,7 @@ statsSpec = describe "HStream.Stats" $ do
 
   it "pre stream stats time series" $ do
     h <- newStatsHolder
+    let intervals = [5 * 1000, 10 * 1000] -- 5, 10 sec
     stream_time_series_add_append_in_bytes h "/topic_1" 1000
     stream_time_series_add_append_in_bytes h "/topic_2" 10000
     -- NOTE: we choose to sleep 1sec so that we can assume the speed of topic_1
@@ -39,17 +40,25 @@ statsSpec = describe "HStream.Stats" $ do
     stream_time_series_add_append_in_bytes h "/topic_1" 1000
     stream_time_series_add_append_in_bytes h "/topic_2" 10000
 
-    m <- stream_time_series_getall_by_name h "appends" [5* 1000, 10 * 1000]  -- 5, 10 sec
+    stream_time_series_get h "appends" "non-existed-stream-name" intervals
+      `shouldReturn` Nothing
 
-    Map.lookup "/topic_1" m `shouldSatisfy` ((\s -> s!!0 > 0 && s!!0 <= 2000) . fromJust)
-    Map.lookup "/topic_1" m `shouldSatisfy` ((\s -> s!!1 > 0 && s!!1 <= 2000) . fromJust)
-    Map.lookup "/topic_2" m `shouldSatisfy` ((\s -> s!!0 > 2000 && s!!0 <= 20000) . fromJust)
-    Map.lookup "/topic_2" m `shouldSatisfy` ((\s -> s!!1 > 2000 && s!!1 <= 20000) . fromJust)
+    Just [rate1_p5s, rate1_p10s] <- stream_time_series_get h "appends" "/topic_1" intervals
+    rate1_p5s `shouldSatisfy` (\s -> s > 0 && s <= 2000)
+    rate1_p10s `shouldSatisfy` (\s -> s > 0 && s <= 2000)
+    Just [rate2_p5s, rate2_p10s] <- stream_time_series_get h "appends" "/topic_2" intervals
+    rate2_p5s `shouldSatisfy` (\s -> s > 2000 && s <= 20000)
+    rate2_p10s `shouldSatisfy` (\s -> s > 2000 && s <= 20000)
+
+    m <- stream_time_series_getall_by_name h "appends" intervals
+    Map.lookup "/topic_1" m `shouldBe` Just [rate1_p5s, rate1_p10s]
+    Map.lookup "/topic_2" m `shouldBe` Just [rate2_p5s, rate2_p10s]
 
 threadedStatsSpec :: Spec
 threadedStatsSpec = describe "HStream.Stats (threaded)" $ do
+  h <- runIO newStatsHolder
+
   it "pre stream stats counter (threaded)" $ do
-    h <- newStatsHolder
     runConc 10 $ runConc 1000 $ do
       stream_stat_add_append_payload_bytes h "a_stream" 1
       stream_stat_add_append_payload_bytes h "b_stream" 1
@@ -63,13 +72,17 @@ threadedStatsSpec = describe "HStream.Stats (threaded)" $ do
     Map.lookup "b_stream" m `shouldBe` Just 10000
 
   it "pre stream stats time series (threaded)" $ do
-    h <- newStatsHolder
     runConc 10 $ runConc 1000 $ do
       stream_time_series_add_append_in_bytes h "a_stream" 1000
       stream_time_series_add_append_in_bytes h "b_stream" 1000
 
     m <- stream_time_series_getall_by_name h "appends" [10 * 1000]  -- 10 sec
     Map.lookup "non-existed-stream-name" m `shouldBe` Nothing
+    stream_time_series_get h "appends" "non-existed-stream-name" [10 * 1000]  -- 10 sec
+      `shouldReturn` Nothing
+
     -- FIXME: Unfortunately, there is no easy way to test with real speed. So we just
     -- check the speed is positive.
     Map.lookup "a_stream" m `shouldSatisfy` ((\s -> head s > 0) . fromJust)
+    stream_time_series_get h "appends" "a_stream" [10 * 1000]
+      `shouldReturn` Map.lookup "a_stream" m
