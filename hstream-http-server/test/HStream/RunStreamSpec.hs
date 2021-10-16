a18d0ba9ded7161b9d77de4fdd93a3f5d6099c67
Alissa Tung <alissa-tung@outlook.com>
2021-10-16 20:09:52 +0800

    rest-api: appends

--- hstream-http-server/test/HStream/RunStreamSpec.hs
+++ hstream-http-server/test/HStream/RunStreamSpec.hs
@@ -1,14 +1,16 @@
+{-# LANGUAGE BlockArguments    #-}
+{-# LANGUAGE LambdaCase        #-}
 {-# LANGUAGE OverloadedStrings #-}
 module HStream.RunStreamSpec (spec) where
 
-import           Data.Aeson                 (decode)
+import           Data.Aeson                 (Value (..), decode)
 import qualified Data.Text                  as T
+import qualified Data.Vector                as V
+import           HStream.HTTP.Server.Stream
+import           HStream.SpecUtils          (appendStream, buildRequest,
+                                             createStream, deleteStream)
 import           Network.HTTP.Simple
 import           Test.Hspec
-
-import           HStream.HTTP.Server.Stream (StreamBO (..))
-import           HStream.SpecUtils          (buildRequest, createStream,
-                                             deleteStream)
 
 -- TODO: config the request url
 listStreams :: IO [StreamBO]
@@ -27,21 +29,34 @@ getStream sName = do
   return (decode (getResponseBody response) :: Maybe StreamBO)
 
 spec :: Spec
-spec = describe "HStream.RunStreamSpec" $ do
+spec = describe "HStream.RunStreamSpec" do
   let sName = "teststream"
 
-  it "create stream" $ do
+  it "create stream" do
     stream <- createStream sName 3
     stream `shouldBe` (Just (StreamBO (T.pack sName) 3))
 
-  it "list streams" $ do
+  it "appends" do
+    AppendResult xs <- appendStream sName testRecords
+    xs `shouldSatisfy` (/= V.empty)
+
+  it "list streams" do
     streams <- listStreams
     (length streams >= 1) `shouldBe` True
 
-  it "get stream" $ do
+  it "get stream" do
     stream <- getStream sName
     stream `shouldBe` (Just (StreamBO (T.pack sName) 3))
 
-  it "delete stream" $ do
+  it "delete stream" do
     res <- deleteStream sName
     res `shouldBe` (Just True)
+
+--------------------------------------------------------------------------------
+testRecords :: Records
+testRecords = buildRecords $ pure
+  [ ("a", Number 42)
+  , ("b", Number 10)
+  , ("c", String "hmm")
+  , ("d", Null)
+  ]
--- hstream-http-server/test/HStream/SpecUtils.hs
+++ hstream-http-server/test/HStream/SpecUtils.hs
@@ -1,13 +1,13 @@
 module HStream.SpecUtils where
 
 import           Data.Aeson                 (decode)
-import           Data.Maybe                 (fromMaybe)
+import           Data.Maybe                 (fromJust, fromMaybe)
 import qualified Data.Text                  as T
 import           Data.Word                  (Word32)
 import           Network.HTTP.Simple
 import           System.Environment         (lookupEnv)
 
-import           HStream.HTTP.Server.Stream (StreamBO (..))
+import           HStream.HTTP.Server.Stream
 
 createStream :: String -> Word32 -> IO (Maybe StreamBO)
 createStream sName rep = do
@@ -15,6 +15,13 @@ createStream sName rep = do
   let request = setRequestBodyJSON (StreamBO (T.pack sName) rep) $ request'
   response <- httpLBS request
   return (decode (getResponseBody response) :: Maybe StreamBO)
+
+appendStream :: String -> Records -> IO AppendResult
+appendStream sName recs = do
+  req <- buildRequest "PUT" $ "streams/" <> sName <> "/publish"
+  let request = setRequestBodyJSON recs req
+  resp <- httpLBS request
+  pure $ fromJust (decode (getResponseBody resp) :: Maybe AppendResult)
 
 deleteStream :: String -> IO (Maybe Bool)
 deleteStream sName = do
