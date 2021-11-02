311e9dd49fdabc5935f666f541649943ed833d04
mu <59917266+4eUeP@users.noreply.github.com>
2021-11-02 16:35:34 +0800

    common: add tests for TimeInterval (#628)

--- common/test/HStream/UtilsSpec.hs
+++ common/test/HStream/UtilsSpec.hs
@@ -2,13 +2,25 @@ module HStream.UtilsSpec (spec) where
 
 import           Control.Concurrent
 import           Control.Monad
+import           Data.Either
 import qualified Data.Set           as Set
 import           HStream.Utils
 import           Test.Hspec
 
 spec :: Spec
-spec = describe "HStream.Utils" $ do
+spec = parallel $ do
+  utilsSpec
+  timeIntervalSpec
 
+timeIntervalSpec :: Spec
+timeIntervalSpec = describe "TimeInterval" $ do
+  it "Parse 1s should be OK" $ do
+    parserInterval "1s" `shouldBe` Right (Seconds 1)
+  it "Parse 1ss should be Err" $ do
+    parserInterval "1ss" `shouldSatisfy` isLeft
+
+utilsSpec :: Spec
+utilsSpec = describe "HStream.Utils" $ do
   it "genUniqueSpec" $ do
     let maxInflights = 5
         eachTimes = 5
