0d7014b18a57c4dbb2a4dce40dede17fda661d8d
mu <59917266+4eUeP@users.noreply.github.com>
2022-03-18 15:15:38 +0800

    store: fix wrong append payload that written to logdevice (#834)

--- hstream-store/HStream/Store/Internal/LogDevice/Writer.hs
+++ hstream-store/HStream/Store/Internal/LogDevice/Writer.hs
@@ -43,7 +43,7 @@ append
 append client logid payload m_key_attr =
   withForeignPtr client $ \client' -> do
   Z.withPrimVectorSafe payload $ \payload' len -> do
-    __append__ client' logid payload' len m_key_attr
+    __append__ client' logid payload' 0 len m_key_attr
 {-# INLINABLE append #-}
 
 appendBS
@@ -53,24 +53,24 @@ appendBS
   -> BS.ByteString
   -> Maybe (KeyType, CBytes)
   -> IO AppendCompletion
-appendBS client logid (BS.PS payload _offset{- always be 0-} len) m_key_attr =
+appendBS client logid (BS.PS payload offset len) m_key_attr =
   withForeignPtr client $ \client' ->
   withForeignPtr payload $ \payload' -> do
-    __append__ client' logid payload' len m_key_attr
+    __append__ client' logid payload' offset len m_key_attr
 {-# INLINABLE appendBS #-}
 
 __append__
   :: HasCallStack
   => Ptr LogDeviceClient
   -> C_LogID
-  -> Ptr Word8 -> Int
+  -> Ptr Word8 -> Int -> Int
   -> Maybe (KeyType, CBytes)
   -> IO AppendCompletion
-__append__ client logid payload len m_key_attr = do
+__append__ client logid payload offset len m_key_attr = do
   cfun <- case m_key_attr of
-            Nothing -> return $ c_logdevice_append_async client logid payload 0 len
+            Nothing -> return $ c_logdevice_append_async client logid payload offset len
             Just (keytype, keyval) -> CBytes.withCBytes keyval $ \keyval' ->
-              return $ c_logdevice_append_with_attrs_async client logid payload 0 len keytype keyval'
+              return $ c_logdevice_append_with_attrs_async client logid payload offset len keytype keyval'
   AppendCallBackData{..} <- withAsync appendCallBackDataSize peekAppendCallBackData cfun
   void $ E.throwStreamErrorIfNotOK' appendCbRetCode
   return $ AppendCompletion appendCbLogID appendCbLSN appendCbTimestamp
