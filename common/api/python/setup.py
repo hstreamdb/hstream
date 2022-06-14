418feb3d99ca76724b96d1b59357f645becf333a
mu <59917266+4eUeP@users.noreply.github.com>
2022-06-14 16:44:44 +0800

    python api: minor updates

--- common/api/python/setup.py
+++ common/api/python/setup.py
@@ -1,11 +1,16 @@
 import setuptools
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setuptools.setup(
     name="hstreamdb-api",
-    version="0.0.2",
+    version="0.0.3",
     author="lambda",
     author_email="lambda@emqx.io",
     description="HStreamDB api for Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/hstreamdb/hstream",
     project_urls={
         "Bug Tracker": "https://github.com/hstreamdb/hstream/issues",
