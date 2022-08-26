24d3516197429dd0aa5cc832735ca1e5627254cf
YangKian <1207783292@qq.com>
2022-08-26 16:16:55 +0800

    support zstd compression algo

--- common/HStream/Utils/Compression.hs
+++ common/HStream/Utils/Compression.hs
@@ -2,11 +2,13 @@ module HStream.Utils.Compression
   ( decompress
   , compress
   , UnknownCompressionType (..)
+  , ZstdCompresstionErr (..)
   )
 where
 
 import qualified Codec.Compression.GZip    as GZ
-import           Control.Exception         (Exception, throw)
+import qualified Codec.Compression.Zstd    as Z
+import           Control.Exception         (Exception, displayException, throw)
 import qualified Data.ByteString           as BS
 import qualified Data.ByteString.Lazy      as BSL
 import           HStream.Server.HStreamApi (CompressionType (..))
@@ -16,6 +18,7 @@ getCompressionType :: Enumerated CompressionType -> Either String CompressionTyp
 getCompressionType tp = case tp of
   (Enumerated (Right CompressionTypeGzip)) -> Right CompressionTypeGzip
   (Enumerated (Right CompressionTypeNone)) -> Right CompressionTypeNone
+  (Enumerated (Right CompressionTypeZstd)) -> Right CompressionTypeZstd
   _                                        -> Left "unknown type"
 
 decompress :: Enumerated CompressionType -> BS.ByteString -> BSL.ByteString
@@ -24,6 +27,10 @@ decompress tp payload =
     in case compressTp of
          Right CompressionTypeGzip -> GZ.decompress . BSL.fromStrict $ payload
          Right CompressionTypeNone -> BSL.fromStrict payload
+         Right CompressionTypeZstd -> case Z.decompress payload of
+           Z.Skip         -> BSL.empty
+           Z.Error e      -> throw $ ZstdCompresstionErr (show e)
+           Z.Decompress s -> BSL.fromStrict s
          Left _                    -> throw UnknownCompressionType
 
 compress :: Enumerated CompressionType -> BSL.ByteString -> BS.ByteString
@@ -32,8 +39,14 @@ compress tp payload =
     in case compressTp of
          Right CompressionTypeGzip -> BSL.toStrict $ GZ.compress payload
          Right CompressionTypeNone -> BSL.toStrict payload
+         Right CompressionTypeZstd -> Z.compress 1 $ BSL.toStrict payload
          Left _                    -> throw UnknownCompressionType
 
 data UnknownCompressionType = UnknownCompressionType
   deriving(Show)
 instance Exception UnknownCompressionType
+
+newtype ZstdCompresstionErr = ZstdCompresstionErr String
+  deriving (Show)
+instance Exception ZstdCompresstionErr where
+  displayException (ZstdCompresstionErr s) = show s
