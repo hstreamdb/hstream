93336ac8cdd465c449bedf4b421e4a5baa4ece9c
s12f <97083380+s12f@users.noreply.github.com>
2022-09-29 16:33:32 +0800

    Hstream io store (#1085)
    feat: refactor hstream io store
    
    Co-authored-by: H⃝oo <56350037+Time-Hu@users.noreply.github.com>

--- hstream-io/HStream/IO/Messages.hs
+++ hstream-io/HStream/IO/Messages.hs
@@ -4,6 +4,7 @@ module HStream.IO.Messages where
 
 import qualified Data.Aeson    as J
 import qualified Data.Aeson.TH as JT
+import           Data.Char     (toLower)
 import qualified Data.Text     as T
 
 
@@ -21,3 +22,48 @@ data CheckResult
     }
 
 $(JT.deriveJSON JT.defaultOptions ''CheckResult)
+
+data ConnectorMessageType
+  = CONNECTOR_CALL
+  | CONNECTOR_SEND
+  deriving (Show)
+
+$(JT.deriveJSON
+    JT.defaultOptions { JT.constructorTagModifier = map toLower }
+    ''ConnectorMessageType)
+
+data KvMessage
+  = KvMessage
+    { kmAction :: T.Text
+    , kmKey    :: T.Text
+    , kmValue  :: Maybe T.Text
+    } deriving (Show)
+
+$(JT.deriveJSON
+    JT.defaultOptions
+      { JT.fieldLabelModifier = map toLower . drop 2 }
+    ''KvMessage)
+
+data ConnectorMessage
+  = ConnectorMessage
+    { cmType    :: ConnectorMessageType
+    , cmId      :: T.Text
+    , cmMessage :: KvMessage
+    } deriving (Show)
+
+$(JT.deriveJSON
+    JT.defaultOptions
+      { JT.fieldLabelModifier = map toLower . drop 2 }
+    ''ConnectorMessage)
+
+data ConnectorCallResponse = ConnectorCallResponse
+  { ccrId      :: T.Text
+  , ccrMessage :: J.Value
+  } deriving (Show)
+
+instance J.ToJSON ConnectorCallResponse where
+  toJSON ConnectorCallResponse{..} =
+    J.object [ ("type" :: T.Text) J..= CONNECTOR_CALL
+             , ("id" :: T.Text) J..= ccrId
+             , ("message" :: T.Text) J..= ccrMessage
+             ]
