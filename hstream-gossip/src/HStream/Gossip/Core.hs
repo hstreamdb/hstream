295209c97fb880031d0a7a5dc9e1a1ff22a6ffc8
mu <59917266+4eUeP@users.noreply.github.com>
2022-10-12 13:47:19 +0800

    try to fix a coredump: use grpc client out of its scope

--- hstream-gossip/src/HStream/Gossip/Core.hs
+++ hstream-gossip/src/HStream/Gossip/Core.hs
@@ -6,52 +6,54 @@
 
 module HStream.Gossip.Core where
 
-import           Control.Concurrent               (killThread, newEmptyMVar,
-                                                   putMVar, readMVar, takeMVar,
-                                                   tryPutMVar)
-import           Control.Concurrent.STM           (atomically, check, dupTChan,
-                                                   flushTQueue, modifyTVar,
-                                                   modifyTVar', newTVarIO,
-                                                   peekTQueue, readTVar,
-                                                   readTVarIO, stateTVar,
-                                                   tryPutTMVar, writeTQueue,
-                                                   writeTVar)
-import           Control.Concurrent.STM.TChan     (readTChan)
-import           Control.Exception                (finally)
-import           Control.Monad                    (forever, join, replicateM,
-                                                   unless, void, when)
-import           Data.Bifunctor                   (bimap)
-import qualified Data.ByteString.Lazy             as BL
-import qualified Data.IntMap.Strict               as IM
-import qualified Data.Map.Strict                  as Map
-import qualified Data.Vector                      as V
-import           Network.GRPC.HighLevel.Generated (withGRPCClient)
-import qualified Proto3.Suite                     as PT
+import           Control.Concurrent             (killThread, newEmptyMVar,
+                                                 putMVar, readMVar, takeMVar,
+                                                 tryPutMVar)
+import           Control.Concurrent.STM         (atomically, check, dupTChan,
+                                                 flushTQueue, modifyTVar,
+                                                 modifyTVar', newTVarIO,
+                                                 peekTQueue, readTVar,
+                                                 readTVarIO, stateTVar,
+                                                 tryPutTMVar, writeTQueue,
+                                                 writeTVar)
+import           Control.Concurrent.STM.TChan   (readTChan)
+import           Control.Exception              (finally)
+import           Control.Monad                  (forever, join, replicateM,
+                                                 unless, void, when)
+import           Data.Bifunctor                 (bimap)
+import qualified Data.ByteString.Lazy           as BL
+import qualified Data.IntMap.Strict             as IM
+import           Data.IORef                     (newIORef, readIORef,
+                                                 writeIORef)
+import qualified Data.Map.Strict                as Map
+import qualified Data.Vector                    as V
+import qualified Proto3.Suite                   as PT
 import qualified SlaveThread
 
-import           HStream.Gossip.Gossip            (gossip)
-import           HStream.Gossip.HStreamGossip     (ServerList (..))
-import           HStream.Gossip.Probe             (doPing, pingReq, pingReqPing)
-import           HStream.Gossip.Types             (EventMessage (EventMessage),
-                                                   EventName, EventPayload,
-                                                   GossipContext (..),
-                                                   InitType (Gossip),
-                                                   RequestAction (..),
-                                                   ServerState (OK, Suspicious),
-                                                   ServerStatus (..),
-                                                   StateMessage (..))
-import qualified HStream.Gossip.Types             as T
-import           HStream.Gossip.Utils             (broadcast, broadcastMessage,
-                                                   cleanStateMessages,
-                                                   eventNameINIT,
-                                                   eventNameINITED, getMsgInc,
-                                                   incrementTVar,
-                                                   mkGRPCClientConf,
-                                                   updateLamportTime,
-                                                   updateStatus)
-import qualified HStream.Logger                   as Log
-import qualified HStream.Server.HStreamInternal   as I
-import           HStream.Utils                    (throwIOError)
+import qualified HStream.Common.GrpcHaskell     as GRPC
+
+import           HStream.Gossip.Gossip          (gossip)
+import           HStream.Gossip.HStreamGossip   (ServerList (..))
+import           HStream.Gossip.Probe           (doPing, pingReq, pingReqPing)
+import           HStream.Gossip.Types           (EventMessage (EventMessage),
+                                                 EventName, EventPayload,
+                                                 GossipContext (..),
+                                                 InitType (Gossip),
+                                                 RequestAction (..),
+                                                 ServerState (OK, Suspicious),
+                                                 ServerStatus (..),
+                                                 StateMessage (..))
+import qualified HStream.Gossip.Types           as T
+import           HStream.Gossip.Utils           (broadcast, broadcastMessage,
+                                                 cleanStateMessages,
+                                                 eventNameINIT, eventNameINITED,
+                                                 getMsgInc, incrementTVar,
+                                                 mkGRPCClientConf,
+                                                 updateLamportTime,
+                                                 updateStatus)
+import qualified HStream.Logger                 as Log
+import qualified HStream.Server.HStreamInternal as I
+import           HStream.Utils                  (throwIOError)
 
 --------------------------------------------------------------------------------
 -- Add a new member to the server list
@@ -65,28 +67,33 @@ addToServerList gc@GossipContext{..} node@I.ServerNode{..} msg state = unless (n
   , latestMessage = initMsg
   , serverState   = initState
   }
-  workersThread <- SlaveThread.fork (joinWorkers gc status)
+  grpcClientRef <- newIORef Nothing
+  let grpcClientFinalizer = do client <- readIORef grpcClientRef
+                               maybe (pure ()) GRPC.deleteGrpcClient client
+  workersThread <- SlaveThread.forkFinally grpcClientFinalizer $ do
+    client <- GRPC.initGrpcClient $ mkGRPCClientConf node
+    writeIORef grpcClientRef (Just client)
+    joinWorkers client gc status
   atomically $ do
     modifyTVar' serverList $ bimap succ (Map.insert serverNodeId status)
     modifyTVar' workers (Map.insert serverNodeId workersThread)
 
-joinWorkers :: GossipContext -> ServerStatus -> IO ()
-joinWorkers gc@GossipContext{..} ss@ServerStatus{serverInfo = sNode@I.ServerNode{..}, ..} = do
-  Log.info . Log.buildString $ "Setting up workers for: " <> show serverNodeId
-  withGRPCClient (mkGRPCClientConf sNode) $ \client -> do
+joinWorkers :: GRPC.Client -> GossipContext -> ServerStatus -> IO ()
+joinWorkers client gc@GossipContext{..} ss@ServerStatus{serverInfo = sNode@I.ServerNode{..}, ..} = do
+    Log.info . Log.buildString $ "Setting up workers for: " <> show serverNodeId
     myChan <- atomically $ dupTChan actionChan
     mvars <- replicateM (T.joinWorkerConcurrency gossipOpts) $ do
       mvar <- newEmptyMVar
       void $ SlaveThread.fork $ do
         finally (forever $ do action <- atomically (readTChan myChan)
-                              doAction client action
+                              doAction action
                 )
                 (putMVar mvar ())
       pure mvar
     -- Normally, this is an infinite block
     mapM_ takeMVar mvars
   where
-    doAction client action = case action of
+    doAction action = case action of
       DoPing sid msg -> when (sid == serverNodeId) $
         doPing client gc ss sid msg
       DoPingReq sids ServerStatus{serverInfo = sInfo} isAcked msg -> when (serverNodeId `elem` sids) $ do
