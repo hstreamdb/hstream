b1590b9fb10a324192e6382a6f487337daa46730
YangKian <1207783292@qq.com>
2023-01-29 12:55:56 +0800

    update

--- hstream/src/HStream/Server/Handler/Stats.hs
+++ hstream/src/HStream/Server/Handler/Stats.hs
@@ -1,6 +1,7 @@
-{-# LANGUAGE DataKinds       #-}
-{-# LANGUAGE GADTs           #-}
-{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE DataKinds         #-}
+{-# LANGUAGE GADTs             #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards   #-}
 
 module HStream.Server.Handler.Stats
   ( -- * For grpc-haskell
@@ -14,6 +15,7 @@ module HStream.Server.Handler.Stats
   ) where
 
 import           Control.Exception                (throwIO)
+import           Data.Functor                     ((<&>))
 import           Data.Int                         (Int64)
 import           Data.Map.Strict                  (Map)
 import qualified Data.Map.Strict                  as Map
@@ -23,8 +25,10 @@ import qualified HsGrpc.Server                    as G
 import           Network.GRPC.HighLevel.Generated
 import qualified Proto3.Suite                     as PS
 
-import           Control.Monad                    (when)
-import           Data.Maybe                       (fromJust, isNothing)
+import           Control.Monad                    (forM, when)
+import           Data.Either                      (partitionEithers)
+import           Data.Maybe                       (mapMaybe)
+import qualified Data.Text                        as T
 import qualified HStream.Exception                as HE
 import qualified HStream.Logger                   as Log
 import           HStream.Server.Exception
@@ -33,6 +37,7 @@ import qualified HStream.Server.HStreamApi        as API
 import           HStream.Stats                    (StatsHolder)
 import qualified HStream.Stats                    as Stats
 import qualified HStream.Utils                    as U
+import           Z.Data.CBytes                    (CBytes)
 
 -------------------------------------------------------------------------------
 
@@ -70,19 +75,15 @@ getStatsHandler
   -> ServerRequest 'Normal API.GetStatsRequest API.GetStatsResponse
   -> IO (ServerResponse 'Normal API.GetStatsResponse)
 getStatsHandler holder (ServerNormalRequest _ (API.GetStatsRequest mstats)) = defaultExceptionHandle $ do
-  let stat = getStats mstats
-  when (isNothing stat) $ throwIO . HE.InvalidStatsType $ show mstats
-  res <- getStatsInternal holder (fromJust stat)
-  U.returnResp $ API.GetStatsResponse {getStatsResponseStatsType = mstats, getStatsResponseStatValues = res}
+  (failed, suc) <- getStats mstats holder
+  U.returnResp $ API.GetStatsResponse {getStatsResponseStatsValues = V.fromList suc, getStatsResponseErrors = V.fromList failed}
 
 handleGetStats
   :: StatsHolder
   -> G.UnaryHandler API.GetStatsRequest API.GetStatsResponse
 handleGetStats holder _ (API.GetStatsRequest mstats) = do
-  let stat = getStats mstats
-  when (isNothing stat) $ throwIO . HE.InvalidStatsType $ show mstats
-  res <- getStatsInternal holder (fromJust stat)
-  pure $ API.GetStatsResponse {getStatsResponseStatsType = mstats, getStatsResponseStatValues = res}
+  (failed, suc) <- getStats mstats holder
+  pure $ API.GetStatsResponse {getStatsResponseStatsValues = V.fromList suc, getStatsResponseErrors = V.fromList failed}
 
 -------------------------------------------------------------------------------
 
@@ -116,54 +117,72 @@ getPerStreamTimeSeriesStatsAll holder req = do
         Left errmsg -> throwIO $ HE.InvalidStatsInterval errmsg
         Right m' -> pure $ Map.map (Just . StatsDoubleVals . V.fromList) . Map.mapKeys U.cBytesToText $ m'
 
-getStatsInternal :: StatsHolder -> StatsTypeStat -> IO (Map Text Int64)
-getStatsInternal holder (StatsTypeStatStreamStat stats) = do
-  getStreamStatsInternal holder stats
-getStatsInternal holder (StatsTypeStatSubStat stats) = do
-  getSubscriptionStatsInternal holder stats
+getStats :: V.Vector StatType -> StatsHolder -> IO ([StatError], [StatValue])
+getStats mstats holder = do
+  let stat = mapMaybe statTypeStat . V.toList $ mstats
+  when (null stat) $ throwIO . HE.InvalidStatsType $ show mstats
+  partitionEithers <$> forM stat (getStatsInternal holder)
+
+getStatsInternal :: StatsHolder -> StatTypeStat -> IO (Either StatError StatValue)
+getStatsInternal holder s@(StatTypeStatStreamStat stats) = do
+  getStreamStatsInternal holder stats <&> convert s
+getStatsInternal holder s@(StatTypeStatSubStat stats) = do
+  getSubscriptionStatsInternal holder stats <&> convert s
 
 getStreamStatsInternal
-  :: Stats.StatsHolder -> PS.Enumerated API.StreamStats -> IO (Map Text Int64)
+  :: Stats.StatsHolder
+  -> PS.Enumerated API.StreamStats
+  -> IO (Either T.Text (Map CBytes Int64))
 getStreamStatsInternal statsHolder (PS.Enumerated stats) = do
   Log.debug $ "request stream stats: " <> Log.buildString' stats
   s <- Stats.newAggregateStats statsHolder
-  res <- case stats of
+  case stats of
     Right API.StreamStatsAppendInBytes ->
-      Stats.stream_stat_getall_append_in_bytes s
+      Stats.stream_stat_getall_append_in_bytes s <&> Right
     Right API.StreamStatsAppendInRecords ->
-      Stats.stream_stat_getall_append_in_records s
-    Right API.StreamStatsTotalAppend ->
-      Stats.stream_stat_getall_append_total s
-    Right API.StreamStatsFailedAppend ->
-      Stats.stream_stat_getall_append_failed s
-    Left _ -> throwIO . HE.InvalidStatsType $ show stats
-  return $ Map.mapKeys U.cBytesToText res
+      Stats.stream_stat_getall_append_in_records s <&> Right
+    Right API.StreamStatsAppendTotal ->
+      Stats.stream_stat_getall_append_total s <&> Right
+    Right API.StreamStatsAppendFailed ->
+      Stats.stream_stat_getall_append_failed s <&> Right
+    Left _ -> return . Left . T.pack $ "invalid stat type " <> show stats
 
 getSubscriptionStatsInternal
   :: Stats.StatsHolder
   -> PS.Enumerated API.SubscriptionStats
-  -> IO (Map Text Int64)
+  -> IO (Either T.Text (Map CBytes Int64))
 getSubscriptionStatsInternal statsHolder (PS.Enumerated stats) = do
   Log.debug $ "request subscription stats: " <> Log.buildString' stats
   s <- Stats.newAggregateStats statsHolder
-  res <- case stats of
-    Right API.SubscriptionStatsDeliveryInBytes ->
-      Stats.subscription_stat_getall_send_out_bytes s
-    Right API.SubscriptionStatsDeliveryInRecords ->
-      Stats.subscription_stat_getall_send_out_records s
-    Right API.SubscriptionStatsAckReceived ->
-      Stats.subscription_stat_getall_received_acks s
+  case stats of
+    Right API.SubscriptionStatsSendOutBytes ->
+      Stats.subscription_stat_getall_send_out_bytes s <&> Right
+    Right API.SubscriptionStatsSendOutRecords ->
+      Stats.subscription_stat_getall_send_out_records s <&> Right
+    Right API.SubscriptionStatsReceivedAcks ->
+      Stats.subscription_stat_getall_received_acks s <&> Right
     Right API.SubscriptionStatsResendRecords ->
-      Stats.subscription_stat_getall_resend_records s
-    Right API.SubscriptionStatsMessageRequestCount ->
-      Stats.subscription_stat_getall_request_messages s
-    Right API.SubscriptionStatsMessageResponseCount ->
-      Stats.subscription_stat_getall_response_messages s
-    Left _ -> throwIO . HE.InvalidStatsType $ show stats
-  return $ Map.mapKeys U.cBytesToText res
+      Stats.subscription_stat_getall_resend_records s <&> Right
+    Right API.SubscriptionStatsResendRecordsFailed ->
+      Stats.subscription_stat_getall_resend_records_failed s <&> Right
+    Right API.SubscriptionStatsRequestMessages ->
+      Stats.subscription_stat_getall_request_messages s <&> Right
+    Right API.SubscriptionStatsResponseMessages ->
+      Stats.subscription_stat_getall_response_messages s <&> Right
+    Left _ -> return . Left . T.pack $ "invalid stat type " <> show stats
 
-getStats :: Maybe StatsType -> Maybe StatsTypeStat
-getStats mstats = do
-  StatsType{..} <- mstats
-  statsTypeStat
+convert :: StatTypeStat -> Either T.Text (Map CBytes Int64) -> Either StatError StatValue
+convert stat (Left msg) = Left $ mkStatError stat msg
+convert stat (Right value) = Right . mkStatValue stat $ Map.mapKeys U.cBytesToText value
+
+mkStatType :: StatTypeStat -> StatType
+mkStatType stat = StatType {statTypeStat = Just stat}
+
+mkStatValue :: StatTypeStat -> Map T.Text Int64 -> StatValue
+mkStatValue stat values =
+  StatValue {statValueStatType = Just . mkStatType $ stat, statValueStatValues = values}
+
+mkStatError :: StatTypeStat -> T.Text -> StatError
+mkStatError stat msg =
+  StatError {statErrorStatType = Just . mkStatType $ stat, statErrorMessage = msg}
 
