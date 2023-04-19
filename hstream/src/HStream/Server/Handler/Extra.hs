0378f10dd3113db920010b905cc2dcc78ef96036
H⃝oo <56350037+Time-Hu@users.noreply.github.com>
2023-04-19 15:01:22 +0800

    add parseSql rpc handlers (#1361)

---
+++ hstream/src/HStream/Server/Handler/Extra.hs
@@ -0,0 +1,34 @@
+{-# LANGUAGE DataKinds  #-}
+{-# LANGUAGE GADTs      #-}
+{-# LANGUAGE LambdaCase #-}
+
+module HStream.Server.Handler.Extra where
+
+import           Control.Exception                (throwIO)
+import qualified HsGrpc.Server                    as G
+import qualified HsGrpc.Server.Types              as G
+import           Network.GRPC.HighLevel.Generated
+
+import qualified HStream.Exception                as HE
+import           HStream.Server.Exception
+import           HStream.Server.HStreamApi        as API
+import           HStream.Server.Types             (ServerContext (..))
+import           HStream.SQL
+import           HStream.Utils
+
+parseSqlHandler
+  :: ServerContext
+  -> ServerRequest 'Normal API.ParseSqlRequest API.ParseSqlResponse
+  -> IO (ServerResponse 'Normal API.ParseSqlResponse)
+parseSqlHandler sc (ServerNormalRequest _metadata req) = defaultExceptionHandle $
+  parseSql req >>= returnResp
+
+handleParseSql :: ServerContext -> G.UnaryHandler API.ParseSqlRequest API.ParseSqlResponse
+handleParseSql sc _ req = catchDefaultEx $ parseSql req
+
+parseSql :: API.ParseSqlRequest  -> IO API.ParseSqlResponse
+parseSql API.ParseSqlRequest{..} = do
+  streamCodegen parseSqlRequestSql >>= \case
+    SelectPlan sources _ _ _ _ -> return $
+      ParseSqlResponse (Just $ ParseSqlResponseSqlEvqSql $ ExecuteViewQuerySql (head sources))
+    _ -> throwIO $ HE.SQLNotSupportedByParseSQL parseSqlRequestSql
