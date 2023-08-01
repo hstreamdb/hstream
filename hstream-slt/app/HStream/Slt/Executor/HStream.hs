485504eeb28ec3537d942811d91234142ace0952
Alissa Tung <alissa-tung@outlook.com>
2023-08-01 15:58:50 +0800

    *: wip

--- hstream-slt/app/HStream/Slt/Executor/HStream.hs
+++ hstream-slt/app/HStream/Slt/Executor/HStream.hs
@@ -2,9 +2,12 @@ module HStream.Slt.Executor.HStream where
 
 import           Control.Monad.IO.Class
 import           Control.Monad.State
+import           HStream.Client.Execute (initCliContext)
 import           HStream.Slt.Executor
 
-data HStreamExecutor
+newtype HStreamExecutor = HStreamExecutor
+  { conn :: HStreamCliContext
+  }
 
 newtype HStreamExecutorCtx executor a = HStreamExecutorCtx {unHStreamExecutorCtx :: ExecutorM executor a}
   deriving (Functor, Applicative, Monad, MonadIO, MonadState (ExecutorState executor))
@@ -23,8 +26,20 @@ instance ExecutorCtx HStreamExecutorCtx executor where
   clearSql = HStreamExecutorCtx clearSql
 
 instance SltExecutor HStreamExecutorCtx HStreamExecutor where
-  open' = undefined
+  open' = open''
   selectWithoutFrom = undefined
   insertValues = undefined
   sqlDataTypeToLiteral' = undefined
   sqlDataValueToLiteral = undefined
+
+----------------------------------------
+
+open'' :: HStreamExecutorCtx HStreamExecutor
+open'' = do
+  opts <- getOpts
+  let refinedOpts :: RefinedCliConnOpts = undefined
+  conn <- liftIO $ initCliContext refinedOpts
+  pure $ HStreamExecutor conn
+  where
+    refineOpts :: GlobalOpts -> RefinedCliConnOpts
+    refineOpts = undefined
