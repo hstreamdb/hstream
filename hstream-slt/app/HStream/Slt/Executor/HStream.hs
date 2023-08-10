0a058bcef71dc2bd72ea8919130eb8c617d894fd
Alissa Tung <alissa-tung@outlook.com>
2023-08-10 15:29:01 +0800

    *: fix fmt

--- hstream-slt/app/HStream/Slt/Executor/HStream.hs
+++ hstream-slt/app/HStream/Slt/Executor/HStream.hs
@@ -2,19 +2,20 @@ module HStream.Slt.Executor.HStream where
 
 import           Control.Monad.IO.Class
 import           Control.Monad.State
-import qualified Data.Text              as T
-import qualified Data.Text.Encoding     as T
-import           HStream.Client.Action
-import           HStream.Client.Execute
+import qualified Data.Text                 as T
+import qualified Data.Text.Encoding        as T
+import qualified Data.Text.IO              as T
+import qualified Data.Vector               as V
+import qualified HStream.Client.Action     as S
+import qualified HStream.Client.Execute    as S
 import           HStream.Client.Types
+import qualified HStream.Server.HStreamApi as API
 import           HStream.Slt.Cli.Parser
 import           HStream.Slt.Executor
 import           HStream.Utils
 
-data HStreamExecutor = HStreamExecutor
-  { conn           :: HStreamCliContext,
-    mainStreamName :: T.Text,
-    groupByIndex   :: Int
+newtype HStreamExecutor = HStreamExecutor
+  { conn :: HStreamCliContext
   }
 
 newtype HStreamExecutorCtx executor a = HStreamExecutorCtx
@@ -49,12 +50,8 @@ instance SltExecutor HStreamExecutorCtx HStreamExecutor where
 open'' :: HStreamExecutorM HStreamExecutor
 open'' = do
   opts <- getOpts
-  debug <- isDebug
-  let refinedOpts :: RefinedCliConnOpts = refineOpts opts
-  conn <- liftIO $ initCliContext refinedOpts
-  streamName <- liftIO $ newRandomText 20
-  liftIO $ createByName debug streamName conn
-  pure $ HStreamExecutor conn streamName 0
+  conn <- liftIO $ S.initCliContext $ refineOpts opts
+  pure $ HStreamExecutor conn
   where
     refineOpts :: GlobalOpts -> RefinedCliConnOpts
     refineOpts GlobalOpts {executorsAddr} =
@@ -72,15 +69,56 @@ open'' = do
       [host, port] -> SocketAddr (T.encodeUtf8 host) (read $ T.unpack port)
       [host]       -> SocketAddr (T.encodeUtf8 host) 6570
       _            -> error "invalid server url for ExecutorKindHStream"
-    createByName :: Bool -> T.Text -> HStreamCliContext -> IO ()
-    createByName debug streamName conn = do
-      when debug $ do
-        putStrLn $ "[DEBUG]: ExecutorHStream create stream " <> T.unpack streamName
-      S.execute_ conn $ createStream streamName 1 (30 * 60)
 
 ----------------------------------------
 
 getConn :: HStreamExecutorM HStreamCliContext
-getConn = do
-  x <- getExecutor
-  _
+getConn = conn <$> getExecutor
+
+execute_ ::
+  Format a =>
+  T.Text ->
+  S.Action a ->
+  HStreamExecutorM ()
+execute_ msg xs = do
+  debug <- isDebug
+  if debug
+    then liftIO $ T.putStrLn $ "[DEBUG]: " <> msg
+    else do
+      conn <- getConn
+      liftIO $ S.execute_ conn xs
+
+executeWithLookupResource ::
+  Format a =>
+  Resource ->
+  (HStreamClientApi -> IO a) ->
+  HStreamExecutorM a
+executeWithLookupResource res xs = do
+  conn <- getConn
+  liftIO $ S.executeWithLookupResource conn res xs
+
+executeViewQuery ::
+  T.Text ->
+  T.Text ->
+  T.Text ->
+  HStreamExecutorM API.ExecuteViewQueryResponse
+executeViewQuery msg res sql = do
+  debug <- isDebug
+  if debug
+    then do
+      liftIO $ T.putStrLn $ "[DEBUG]: " <> msg
+      pure $ API.ExecuteViewQueryResponse V.empty
+    else do
+      x <- executeWithLookupResource (Resource ResView res) $ S.executeViewQuery $ T.unpack sql
+      liftIO $ getServerResp x
+
+createStream :: T.Text -> HStreamExecutorM ()
+createStream streamName = do
+  execute_
+    ("ExecutorHStream create stream " <> streamName)
+    $ S.createStream streamName 1 (30 * 60)
+
+createView :: T.Text -> HStreamExecutorM ()
+createView sql = do
+  qName <- ("slt_generated_" <>) <$> newRandomText 10
+  S.executeWithLookupResource_ conn (Resource ResQuery qName) (createStreamBySelectWithCustomQueryName sql qName)
