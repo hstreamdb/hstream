5697d69a05d769ae5e217add2bfbefdd3b99f6eb
mu <59917266+4eUeP@users.noreply.github.com>
2023-09-06 15:17:16 +0800

    kafka: fix Metadata handler
    * returned leaderId
    * return all topics when reqTopics is `Nothing`

--- hstream/src/HStream/Server/KafkaHandler/Basic.hs
+++ hstream/src/HStream/Server/KafkaHandler/Basic.hs
@@ -8,19 +8,23 @@ module HStream.Server.KafkaHandler.Basic
   , handleMetadataV1
   ) where
 
+import           Control.Concurrent.STM        (readTVarIO)
 import           Control.Exception
 import           Control.Monad
+import           Data.Functor                  ((<&>))
 import           Data.Int                      (Int32)
-import qualified Data.List                     as L
-import qualified Data.Map.Strict               as M
 import           Data.Text                     (Text)
+import qualified Data.Text                     as Text
 import qualified Data.Vector                   as V
 
+import qualified HStream.Logger                as Log
 import qualified HStream.Server.Core.Cluster   as Core
+import qualified HStream.Server.Core.Common    as Core
 import qualified HStream.Server.HStreamApi     as GRPC
-import qualified HStream.Server.Types          as HsTypes
+import           HStream.Server.Types          (ServerContext (..),
+                                                transToStreamName)
 import qualified HStream.Store                 as S
-
+import qualified HStream.Utils                 as Utils
 import qualified Kafka.Protocol.Error          as K
 import qualified Kafka.Protocol.Message.Struct as K
 import qualified Kafka.Protocol.Service        as K
@@ -48,7 +52,7 @@ handleApiversionsV2 = handleApiversionsV1
 --  3: Metadata
 --------------------
 handleMetadataV0
-  :: HsTypes.ServerContext -> K.RequestContext -> K.MetadataRequestV0 -> IO K.MetadataResponseV0
+  :: ServerContext -> K.RequestContext -> K.MetadataRequestV0 -> IO K.MetadataResponseV0
 handleMetadataV0 ctx reqCtx req = do
   (K.MetadataResponseV1 brokers _ topics) <- handleMetadataV1 ctx reqCtx req
   return $ K.MetadataResponseV0 (V.map respBrokerV1toV0 <$> brokers)
@@ -64,18 +68,20 @@ handleMetadataV0 ctx reqCtx req = do
       K.MetadataResponseTopicV0 errorCode name partitions
 
 handleMetadataV1
-  :: HsTypes.ServerContext -> K.RequestContext -> K.MetadataRequestV1 -> IO K.MetadataResponseV1
-handleMetadataV1 ctx _ req = do
+  :: ServerContext -> K.RequestContext -> K.MetadataRequestV1 -> IO K.MetadataResponseV1
+handleMetadataV1 ctx@ServerContext{..} _ req = do
   respBrokers <- getBrokers
-  let ctlId = fromIntegral (HsTypes.serverID ctx)
+  -- FIXME: `serverID` is a `Word32` but kafka expects an `Int32`,
+  -- causing a potential overflow.
+  let ctlId = fromIntegral serverID
   let (K.MetadataRequestV0 reqTopics) = req
   case reqTopics of
-    Nothing ->
-      -- FIXME: We return `[]` when reqTopics is `Nothing`.
-      --        Is this proper?
-      -- FIXME: `serverID` is a `Word64` but kafka expects an `Int32`,
-      --        causing a potential overflow.
-      return $ K.MetadataResponseV1 (Just respBrokers) (fromIntegral $ HsTypes.serverID ctx) (Just V.empty)
+    Nothing -> do
+      -- FIXME: `serverID` is a `Word32` but kafka expects an `Int32`,
+      -- causing a potential overflow.
+      allStreamNames <- S.findStreams scLDClient S.StreamTypeStream <&> (fmap (Utils.cBytesToText . S.streamName))
+      respTopics <- forM allStreamNames getRespTopic <&> V.fromList
+      return $ K.MetadataResponseV1 (Just respBrokers) ctlId (Just respTopics)
     Just v  -> do
       let topicNames = V.map (\K.MetadataRequestTopicV0{..} -> name) v
       respTopics <- forM topicNames getRespTopic
@@ -96,28 +102,37 @@ handleMetadataV1 ctx _ req = do
 
     getRespTopic :: Text -> IO K.MetadataResponseTopicV1
     getRespTopic topicName = do
-      let streamId = HsTypes.transToStreamName topicName
-      shards_e <- try (M.elems <$> S.listStreamPartitions (HsTypes.scLDClient ctx) streamId)
+      let streamId = transToStreamName topicName
+      shards_e <- try ((V.map snd) <$> S.listStreamPartitionsOrdered scLDClient streamId)
       case shards_e of
         -- FIXME: We catch all exceptions here. Is this proper?
         -- FIXME: Are the following error codes proper?
         -- FIXME: We passed `Nothing` as partitions when an error occurs. Is this proper?
         Left (_ :: SomeException) ->
-          return $ K.MetadataResponseTopicV1 K.INVALID_TOPIC_EXCEPTION topicName False Nothing
+          return $ K.MetadataResponseTopicV1 K.UNKNOWN_TOPIC_OR_PARTITION topicName False Nothing
         Right shards
-          | length shards == 0 ->
-            return $ K.MetadataResponseTopicV1 K.INVALID_TOPIC_EXCEPTION topicName False Nothing
-          | length shards > fromIntegral (maxBound :: Int32) ->
-            return $ K.MetadataResponseTopicV1 K.INVALID_PARTITIONS topicName False Nothing
+          | V.null shards ->
+              return $ K.MetadataResponseTopicV1 K.INVALID_TOPIC_EXCEPTION topicName False Nothing
+          | V.length shards > fromIntegral (maxBound :: Int32) ->
+              return $ K.MetadataResponseTopicV1 K.INVALID_PARTITIONS topicName False Nothing
           | otherwise -> do
-              let respPartitions =
-                    L.map (\shardId -> K.MetadataResponsePartitionV0
-                                       { errorCode      = K.NONE
-                                       , partitionIndex = fromIntegral shardId
-                                       , leaderId       = 0 -- FIXME: hardcoded
-                                       , replicaNodes   = Just (V.singleton 0) -- FIXME: hardcoded
-                                       , isrNodes       = Just (V.singleton 0) -- FIXME: hardcoded
-                                       }
-                          ) shards
+              respPartitions <-
+                V.iforM shards $ \idx shardId -> do
+                  -- Note: we choose the leader using the same method as `lookupShard` of the old GRPC server.
+                  (_, hashRing) <- readTVarIO loadBalanceHashRing
+                  theNode <- Core.getResNode hashRing (Text.pack $ show shardId) scAdvertisedListenersKey
+                  -- FIXME: Convert from `Word32` to `Int32`, possible overflow!
+                  when ((GRPC.serverNodeId theNode) > fromIntegral (maxBound :: Int32)) $
+                    Log.warning $ "ServerID " <> Log.build (GRPC.serverNodeId theNode)
+                               <> " is too large, it should be less than "
+                               <> Log.build (maxBound :: Int32)
+                  let (theNodeId :: Int32) = fromIntegral (GRPC.serverNodeId theNode)
+                  pure $ K.MetadataResponsePartitionV0
+                           { errorCode      = K.NONE
+                           , partitionIndex = (fromIntegral idx)
+                           , leaderId       = theNodeId
+                           , replicaNodes   = Just (V.singleton 0) -- FIXME: hardcoded
+                           , isrNodes       = Just (V.singleton 0) -- FIXME: hardcoded
+                           }
               return $
-                K.MetadataResponseTopicV1 K.NONE topicName False (Just $ V.fromList respPartitions)
+                K.MetadataResponseTopicV1 K.NONE topicName False (Just respPartitions)
