2540f119f2729e5cf1cc57c88f0460b6cd6d38a5
mu <59917266+4eUeP@users.noreply.github.com>
2023-09-12 16:01:42 +0800

    kafka: a naive Produce handler

--- hstream-kafka/src/Kafka/Common/RecordFormat.hs
+++ hstream-kafka/src/Kafka/Common/RecordFormat.hs
@@ -2,17 +2,16 @@ module Kafka.Common.RecordFormat
   ( RecordFormat (..)
   ) where
 
-import           Data.ByteString         (ByteString)
 import           Data.Int
 import           GHC.Generics            (Generic)
 
 import qualified Kafka.Protocol.Encoding as K
 
--- Format to store in logdevice
+-- on-disk format
 data RecordFormat = RecordFormat
   { offset      :: Int64
   , batchLength :: Int32
-  , recordBytes :: ByteString
+  , recordBytes :: K.CompactBytes
   } deriving (Generic, Show)
 
 instance K.Serializable RecordFormat
