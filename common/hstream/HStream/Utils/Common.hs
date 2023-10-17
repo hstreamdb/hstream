e4122df41cb5d757134c5f403a564e55832fc1a1
YangKian <1207783292@qq.com>
2023-10-17 15:32:05 +0800

    move limitedMapConcurrently func to HStream.Utils.Common

--- common/hstream/HStream/Utils/Common.hs
+++ common/hstream/HStream/Utils/Common.hs
@@ -1,10 +1,14 @@
 module HStream.Utils.Common
   ( maybeToEither
   , newRandomText
+  , limitedMapConcurrently
   ) where
 
-import           Data.Text     (Text)
-import qualified Data.Text     as Text
+import           Control.Concurrent.Async (mapConcurrently)
+import           Control.Concurrent.QSem  (QSem, newQSem, signalQSem, waitQSem)
+import           Control.Exception        (bracket_)
+import           Data.Text                (Text)
+import qualified Data.Text                as Text
 import           System.Random
 
 maybeToEither :: b -> Maybe a -> Either b a
@@ -12,3 +16,11 @@ maybeToEither errmsg = maybe (Left errmsg) Right
 
 newRandomText :: Int -> IO Text
 newRandomText n = Text.pack . take n . randomRs ('a', 'z') <$> newStdGen
+
+limitedMapConcurrently :: Int -> (a -> IO b) -> [a] -> IO [b]
+limitedMapConcurrently maxConcurrency f inputs = do
+  sem <- newQSem maxConcurrency
+  mapConcurrently (limited sem . f) inputs
+ where
+   limited :: QSem -> IO c -> IO c
+   limited sem = bracket_ (waitQSem sem) (signalQSem sem)
