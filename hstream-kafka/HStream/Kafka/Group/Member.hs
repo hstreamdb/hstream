e78bb3970ab00c5460bfb9b3c3c4d512c80114e8
s12f <siwen.feng@emqx.io>
2023-11-03 10:52:36 +0800

    kafka: rebalancing without reset group

--- hstream-kafka/HStream/Kafka/Group/Member.hs
+++ hstream-kafka/HStream/Kafka/Group/Member.hs
@@ -12,34 +12,52 @@ import           Data.Int               (Int32, Int64)
 import qualified Data.IORef             as IO
 import           Data.Maybe             (fromMaybe)
 import qualified Data.Text              as T
+import qualified Kafka.Protocol         as K
 import qualified Kafka.Protocol.Service as K
 
 data Member
   = Member
-  { memberId          :: T.Text
-  , sessionTimeoutMs  :: Int32
-  , assignment        :: IO.IORef BS.ByteString
-  , lastHeartbeat     :: IO.IORef Int64
-  , heartbeatThread   :: IO.IORef (Maybe C.ThreadId)
+  { memberId           :: T.Text
+  , rebalanceTimeoutMs :: IO.IORef Int32
+  , sessionTimeoutMs   :: IO.IORef Int32
+  , assignment         :: IO.IORef BS.ByteString
+  , lastHeartbeat      :: IO.IORef Int64
+  , heartbeatThread    :: IO.IORef (Maybe C.ThreadId)
 
   -- protocols
-  , protocolType      :: T.Text
-  , supportedProtcols :: [(T.Text, BS.ByteString)]
+  , protocolType       :: T.Text
+  , supportedProtocols :: IO.IORef [(T.Text, BS.ByteString)]
 
   -- client information
-  , clientId          :: T.Text
-  , clientHost        :: T.Text
+  , clientId           :: T.Text
+  , clientHost         :: T.Text
   }
 
-newMember :: K.RequestContext -> T.Text -> Int32 -> T.Text -> [(T.Text, BS.ByteString)] -> IO Member
-newMember reqCtx memberId sessionTimeoutMs protocolType supportedProtcols = do
+newMemberFromReq :: K.RequestContext -> K.JoinGroupRequestV0 -> T.Text -> [(T.Text, BS.ByteString)] -> IO Member
+newMemberFromReq reqCtx req memberId supportedProtocols = do
+  sessionTimeoutMs <- IO.newIORef req.sessionTimeoutMs
+  rebalanceTimeoutMs <- IO.newIORef req.sessionTimeoutMs
+
   assignment <- IO.newIORef BS.empty
+
   lastHeartbeat <- IO.newIORef 0
   heartbeatThread <- IO.newIORef Nothing
 
-  -- TODO: read from request context
-  let clientId = fromMaybe "" (join reqCtx.clientId)
-      clientHost = T.pack reqCtx.clientHost
+  supportedProtocols' <- IO.newIORef supportedProtocols
 
-  -- TODO: check request
-  return $ Member {..}
+  return $ Member {
+      memberId=memberId
+    , rebalanceTimeoutMs=rebalanceTimeoutMs
+    , sessionTimeoutMs=sessionTimeoutMs
+
+    , assignment=assignment
+
+    , lastHeartbeat=lastHeartbeat
+    , heartbeatThread=heartbeatThread
+
+    , protocolType=req.protocolType
+    , supportedProtocols=supportedProtocols'
+
+    , clientId=fromMaybe "" (join reqCtx.clientId)
+    , clientHost=T.pack reqCtx.clientHost
+    }
