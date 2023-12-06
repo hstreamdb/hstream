8bb3304447581c90d7ace8c7870b192d301dbb3c
mu <59917266+4eUeP@users.noreply.github.com>
2023-12-06 14:29:32 +0800

    breaking change: add version in on-disk format

--- hstream-kafka/HStream/Kafka/Common/RecordFormat.hs
+++ hstream-kafka/HStream/Kafka/Common/RecordFormat.hs
@@ -14,9 +14,10 @@ import qualified Kafka.Protocol.Encoding as K
 
 -- on-disk format
 data RecordFormat = RecordFormat
-  { offset      :: Int64
-  , batchLength :: Int32
-  , recordBytes :: K.CompactBytes
+  { version     :: {-# UNPACK #-} !Int8
+  , offset      :: {-# UNPACK #-} !Int64
+  , batchLength :: {-# UNPACK #-} !Int32
+  , recordBytes :: !K.CompactBytes
   } deriving (Generic, Show)
 
 instance K.Serializable RecordFormat
@@ -24,7 +25,7 @@ instance K.Serializable RecordFormat
 seekBatch :: Int32 -> ByteString -> IO ByteString
 seekBatch i bs =
   let parser = replicateM_ (fromIntegral i) $ do
-                 void $ K.takeBytes 8{- baseOffset: Int64 -}
+                 void $ K.takeBytes 9{- version(1) + offset(8) -}
                  len <- K.get @Int32
                  void $ K.takeBytes (fromIntegral len)
    in snd <$> K.runParser' parser bs
