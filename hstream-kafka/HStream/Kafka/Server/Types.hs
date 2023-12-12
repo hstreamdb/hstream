a5d4e50789cc3639e8c0d197918928d0e8e8e3a8
mu <59917266+4eUeP@users.noreply.github.com>
2023-12-12 16:44:30 +0800

    kafka: using a per-connection reader to read records

--- hstream-kafka/HStream/Kafka/Server/Types.hs
+++ hstream-kafka/HStream/Kafka/Server/Types.hs
@@ -1,15 +1,19 @@
 module HStream.Kafka.Server.Types
   ( ServerContext (..)
   , initServerContext
+  , initConnectionContext
   ) where
 
 import           Data.Text                               (Text)
 import           Data.Word
+import           Foreign.ForeignPtr                      (newForeignPtr_)
+import           Foreign.Ptr                             (nullPtr)
 
 import           HStream.Common.Server.HashRing          (LoadBalanceHashRing,
                                                           initializeHashRing)
 import           HStream.Gossip.Types                    (GossipContext)
 import           HStream.Kafka.Common.OffsetManager      (OffsetManager,
+                                                          initOffsetReader,
                                                           newOffsetManager)
 import           HStream.Kafka.Group.GroupCoordinator    (GroupCoordinator,
                                                           mkGroupCoordinator)
@@ -31,10 +35,13 @@ data ServerContext = ServerContext
   , cmpStrategy              :: !S.Compression
   , loadBalanceHashRing      :: !LoadBalanceHashRing
   , gossipContext            :: !GossipContext
-  , scOffsetManager          :: !OffsetManager
-  , scGroupCoordinator       :: GroupCoordinator
+  , scGroupCoordinator       :: !GroupCoordinator
   , kafkaBrokerConfigs       :: !KC.KafkaBrokerConfigs
-}
+    -- { per connection, see 'initConnectionContext'
+  , scOffsetManager          :: !OffsetManager
+  , fetchReader              :: !S.LDReader
+    -- } per connection end
+  }
 
 initServerContext
   :: ServerOpts
@@ -50,8 +57,11 @@ initServerContext opts@ServerOpts{..} gossipContext mh = do
   -- XXX: Should we add a server option to toggle Stats?
   statsHolder <- newServerStatsHolder
   epochHashRing <- initializeHashRing gossipContext
-  offsetManager <- newOffsetManager ldclient
   scGroupCoordinator <- mkGroupCoordinator mh ldclient _serverID
+
+  offsetManager <- newOffsetManager ldclient
+  -- Trick to avoid use maybe, must be initialized later
+  fetchReader <- newForeignPtr_ nullPtr
 
   return
     ServerContext
@@ -65,7 +75,32 @@ initServerContext opts@ServerOpts{..} gossipContext mh = do
       , cmpStrategy              = _compression
       , loadBalanceHashRing      = epochHashRing
       , gossipContext            = gossipContext
-      , scOffsetManager          = offsetManager
       , scGroupCoordinator       = scGroupCoordinator
-      , kafkaBrokerConfigs = _kafkaBrokerConfigs
+      , kafkaBrokerConfigs       = _kafkaBrokerConfigs
+      , scOffsetManager          = offsetManager
+      , fetchReader              = fetchReader
       }
+
+initConnectionContext :: ServerContext -> IO ServerContext
+initConnectionContext sc = do
+  -- Since the Reader inside OffsetManger is thread-unsafe, for each connection
+  -- we create a new Reader.
+  !om <- initOffsetReader $ scOffsetManager sc
+
+  -- Reader used for fetch.
+  --
+  -- Currently, we only need one reader per connection because there will be
+  -- only one thread to fetch data.
+  --
+  -- TODO: also considering the following:
+  --
+  -- - use a pool of readers.
+  -- - create a reader(or pool of readers) for each consumer group.
+  --
+  -- NOTE: the maxLogs is set to 1000, which means the reader will fetch at most
+  -- 1000 logs.
+  -- TODO: maybe we should set maxLogs dynamically according to the max number
+  -- of all fetch requests in this connection.
+  !reader <- S.newLDReader (scLDClient sc) 1000{-maxLogs-} (Just 10){-bufferSize-}
+
+  pure sc{scOffsetManager = om, fetchReader = reader}
