4f688cab13a8c53095eb622e638873af1deade3c
Commelina <maosics@gmail.com>
2024-03-06 18:04:02 +0800

    kafka: do authorization on create/delete topics & create partitions (#1771)

--- hstream-kafka/HStream/Kafka/Server/Handler/Topic.hs
+++ hstream-kafka/HStream/Kafka/Server/Handler/Topic.hs
@@ -15,24 +15,29 @@ module HStream.Kafka.Server.Handler.Topic
 
 import           Control.Exception
 import           Control.Monad
-import qualified Data.List                          as L
-import qualified Data.Map                           as Map
-import           Data.Maybe                         (isNothing)
-import qualified Data.Text                          as T
-import qualified Data.Vector                        as V
+import           Control.Monad.Extra                   ((&&^))
+import           Data.Functor                          ((<&>))
+import qualified Data.List                             as L
+import qualified Data.Map                              as Map
+import           Data.Maybe                            (isNothing)
+import qualified Data.Text                             as T
+import qualified Data.Vector                           as V
 
-import           HStream.Kafka.Common.OffsetManager (cleanOffsetCache)
-import qualified HStream.Kafka.Common.Utils         as Utils
-import qualified HStream.Kafka.Server.Core.Topic    as Core
-import           HStream.Kafka.Server.Types         (ServerContext (..))
-import qualified HStream.Logger                     as Log
-import qualified HStream.Store                      as S
-import           Kafka.Protocol                     (NullableString)
-import qualified Kafka.Protocol.Encoding            as K
-import           Kafka.Protocol.Error               (ErrorCode)
-import qualified Kafka.Protocol.Error               as K
-import qualified Kafka.Protocol.Message             as K
-import qualified Kafka.Protocol.Service             as K
+import           HStream.Kafka.Common.Acl
+import           HStream.Kafka.Common.Authorizer.Class
+import           HStream.Kafka.Common.OffsetManager    (cleanOffsetCache)
+import           HStream.Kafka.Common.Resource
+import qualified HStream.Kafka.Common.Utils            as Utils
+import qualified HStream.Kafka.Server.Core.Topic       as Core
+import           HStream.Kafka.Server.Types            (ServerContext (..))
+import qualified HStream.Logger                        as Log
+import qualified HStream.Store                         as S
+import           Kafka.Protocol                        (NullableString)
+import qualified Kafka.Protocol.Encoding               as K
+import           Kafka.Protocol.Error                  (ErrorCode)
+import qualified Kafka.Protocol.Error                  as K
+import qualified Kafka.Protocol.Message                as K
+import qualified Kafka.Protocol.Service                as K
 
 --------------------
 -- 19: CreateTopics
@@ -40,7 +45,7 @@ import qualified Kafka.Protocol.Service             as K
 -- FIXME: The `timeoutMs` field of request is omitted.
 handleCreateTopics
   :: ServerContext -> K.RequestContext -> K.CreateTopicsRequest -> IO K.CreateTopicsResponse
-handleCreateTopics ctx@ServerContext{scLDClient} _ K.CreateTopicsRequest{..} =
+handleCreateTopics ctx@ServerContext{scLDClient} reqCtx K.CreateTopicsRequest{..} =
   case topics of
     K.KaArray Nothing ->
       -- FIXME: We return `[]` when topics is `Nothing`.
@@ -49,7 +54,9 @@ handleCreateTopics ctx@ServerContext{scLDClient} _ K.CreateTopicsRequest{..} =
     K.KaArray (Just topics_)
       | V.null topics_ -> return $ K.CreateTopicsResponse {topics = K.KaArray $ Just V.empty, throttleTimeMs = 0}
       | otherwise      -> do
-          let (errRes, topics') = V.partitionWith (\tp -> mapErr tp.name . validateTopic $ tp) topics_
+          (errRes, topics') <- mapM (\tp -> mapErr tp.name
+                                        <$> liftM2 (*>) (authorizeTopic tp) (pure . validateTopic $ tp)
+                                    ) topics_ <&> V.partitionWith id
           if | null topics' ->
                 -- all topics validate failed, return directly
                 return $ K.CreateTopicsResponse {topics = K.KaArray $ Just errRes, throttleTimeMs = 0}
@@ -66,20 +73,34 @@ handleCreateTopics ctx@ServerContext{scLDClient} _ K.CreateTopicsRequest{..} =
                          , name=name
                          }
                     else return K.CreatableTopicResult {errorMessage=Nothing, errorCode=K.NONE, name=name}
-
+                -- FIXME: Topics of response have different order from request's. Is this allowed?
                 return $ K.CreateTopicsResponse {topics = K.KaArray . Just $ res <> errRes, throttleTimeMs = 0}
              | otherwise -> do
                 respTopics <- forM topics' $ createTopic
+                -- FIXME: Topics of response have different order from request's. Is this allowed?
                 return $ K.CreateTopicsResponse {topics = K.KaArray . Just $ respTopics <> errRes, throttleTimeMs = 0}
   where
+    -- FIXME: also check [CREATE CLUSTER], which implies [CREATE TOPIC]
+    -- [ACL] authorize a topic for [CREATE TOPIC] and [DESCRIBE_CONFIG TOPIC]
+    authorizeTopic :: K.CreatableTopic -> IO (Either (ErrorCode, NullableString) K.CreatableTopic)
+    authorizeTopic topic = do
+      authzed <- simpleAuthorize (toAuthorizableReqCtx reqCtx) ctx.authorizer Res_TOPIC topic.name AclOp_CREATE
+              &&^ simpleAuthorize (toAuthorizableReqCtx reqCtx) ctx.authorizer Res_TOPIC topic.name AclOp_DESCRIBE_CONFIGS
+      if authzed
+        then return (Right topic)
+        else return (Left (K.TOPIC_AUTHORIZATION_FAILED, Just $ "Authorization failed."))
+
     mapErr name (Left (errorCode, msg)) = Left $ K.CreatableTopicResult name errorCode msg
     mapErr _ (Right tp) = Right tp
 
     createTopic :: K.CreatableTopic -> IO K.CreatableTopicResult
-    createTopic K.CreatableTopic{..} = do
-      let configMap = Map.fromList . map (\c -> (c.name, c.value)) . Utils.kaArrayToList $ configs
-      ((errorCode, msg), _) <- Core.createTopic ctx name replicationFactor numPartitions configMap
-      return $ K.CreatableTopicResult name errorCode (Just msg)
+    createTopic topic@K.CreatableTopic{..} = do
+      authorizeTopic topic >>= \case
+        Left (errCode, msg) -> return $ K.CreatableTopicResult name errCode msg
+        Right _             -> do
+          let configMap = Map.fromList . map (\c -> (c.name, c.value)) . Utils.kaArrayToList $ configs
+          ((errorCode, msg), _) <- Core.createTopic ctx name replicationFactor numPartitions configMap
+          return $ K.CreatableTopicResult name errorCode (Just msg)
 
 validateTopic :: K.CreatableTopic -> Either (ErrorCode, NullableString) K.CreatableTopic
 validateTopic topic@K.CreatableTopic{..} = do
@@ -118,7 +139,7 @@ validateTopic topic@K.CreatableTopic{..} = do
 -- FIXME: The `timeoutMs` field of request is omitted.
 handleDeleteTopics
   :: ServerContext -> K.RequestContext -> K.DeleteTopicsRequest -> IO K.DeleteTopicsResponse
-handleDeleteTopics ServerContext{..} _ K.DeleteTopicsRequest{..} =
+handleDeleteTopics ServerContext{..} reqCtx K.DeleteTopicsRequest{..} =
   case topicNames of
     K.KaArray Nothing ->
       -- FIXME: We return `[]` when topics is `Nothing`.
@@ -128,15 +149,24 @@ handleDeleteTopics ServerContext{..} _ K.DeleteTopicsRequest{..} =
       | V.null topicNames_ -> return $ K.DeleteTopicsResponse {responses = K.KaArray $ Just V.empty, throttleTimeMs = 0}
       | otherwise          -> do
           respTopics <- forM topicNames_ $ \topicName -> do
-            try (deleteTopic topicName) >>= \case
-              Left (e :: SomeException)
-                | Just _ <- fromException @S.NOTFOUND e -> do
-                   Log.warning $ "Delete topic failed, topic " <> Log.build topicName <> " does not exist"
-                   return $ K.DeletableTopicResult topicName K.UNKNOWN_TOPIC_OR_PARTITION
-                | otherwise -> do
-                    Log.warning $ "Exception occurs when deleting topic " <> Log.build topicName <> ": " <> Log.build (show e)
-                    return $ K.DeletableTopicResult topicName K.UNKNOWN_SERVER_ERROR
-              Right res -> return res
+            -- [ACL] check [DESCRIBE TOPIC] and [DELETE TOPIC]
+            simpleAuthorize (toAuthorizableReqCtx reqCtx) authorizer Res_TOPIC topicName AclOp_DESCRIBE >>= \case
+              -- Note: According to Kafka's implementation,
+              --       if there is no DESCRIBE permission, the return topic name should be null.
+              --       See kafka.server.KafkaApis#handleDeleteTopicsRequest
+              False -> return $ K.DeletableTopicResult "" K.TOPIC_AUTHORIZATION_FAILED
+              True  -> simpleAuthorize (toAuthorizableReqCtx reqCtx) authorizer Res_TOPIC topicName AclOp_DELETE >>= \case
+                False -> return $ K.DeletableTopicResult topicName K.TOPIC_AUTHORIZATION_FAILED
+                True  -> do
+                  try (deleteTopic topicName) >>= \case
+                    Left (e :: SomeException)
+                      | Just _ <- fromException @S.NOTFOUND e -> do
+                         Log.warning $ "Delete topic failed, topic " <> Log.build topicName <> " does not exist"
+                         return $ K.DeletableTopicResult topicName K.UNKNOWN_TOPIC_OR_PARTITION
+                      | otherwise -> do
+                          Log.warning $ "Exception occurs when deleting topic " <> Log.build topicName <> ": " <> Log.build (show e)
+                          return $ K.DeletableTopicResult topicName K.UNKNOWN_SERVER_ERROR
+                    Right res -> return res
           return $ K.DeleteTopicsResponse {responses = K.KaArray $ Just respTopics, throttleTimeMs = 0}
   where
     -- FIXME: There can be some potential exceptions which are difficult to
@@ -163,18 +193,21 @@ handleDeleteTopics ServerContext{..} _ K.DeleteTopicsRequest{..} =
 --------------------
 handleCreatePartitions
   :: ServerContext -> K.RequestContext -> K.CreatePartitionsRequest -> IO K.CreatePartitionsResponse
-handleCreatePartitions ctx _ req@K.CreatePartitionsRequest{..} = do
+handleCreatePartitions ctx reqCtx req@K.CreatePartitionsRequest{..} = do
   let topics' = Utils.kaArrayToVector req.topics
       groups = L.groupBy (\a b -> a.name == b.name) . L.sortBy (\a b -> compare a.name b.name) $ V.toList topics'
       dups = concat $ L.filter (\l -> length l > 1) groups
       validReques = concat $ L.filter (\l -> length l == 1) groups
-
-  succResults <- forM validReques $ \K.CreatePartitionsTopic{..} -> do
-    res <- Core.createPartitions ctx name count assignments timeoutMs validateOnly
-    return $ mkResults name res
+  validResults <- forM validReques $ \K.CreatePartitionsTopic{..} -> do
+    -- [ACL] check [ALTER TOPIC]
+    simpleAuthorize (toAuthorizableReqCtx reqCtx) ctx.authorizer Res_TOPIC name AclOp_ALTER >>= \case
+      False -> return $ mkResults name (K.TOPIC_AUTHORIZATION_FAILED, "The topic authorization is failed.")
+      True  -> do
+        res <- Core.createPartitions ctx name count assignments timeoutMs validateOnly
+        return $ mkResults name res
   let dupsResults = map (\t -> mkResults t.name (K.INVALID_REQUEST, "Duplicate topic in request.")) dups
   return K.CreatePartitionsResponse
-        { results = K.KaArray . Just . V.fromList $ succResults <> dupsResults
+        { results = K.KaArray . Just . V.fromList $ validResults <> dupsResults
         , throttleTimeMs = 0
         }
  where
